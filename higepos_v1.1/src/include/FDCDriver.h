81fca29f16d563625cc3d770eb5ea30f2d9e4b09
syn <none>
2002-11-02 15:22:23 +0000

    rollbacl to 1.2

--- higepos_v1.1/src/include/FDCDriver.h
+++ higepos_v1.1/src/include/FDCDriver.h
@@ -22,9 +22,7 @@ class FDCDriver{
     int seek(const unsigned int head, const unsigned int track);
     int readSector(const unsigned long start_sector, const unsigned long num_sector, unsigned char buff[]);
     int readSector(const int h, const int c, const int r, const int num_sector, unsigned char buff[]);
-    int readSector(const int h, const int c, const int r, unsigned char buff[]);
     int writeSector(const unsigned long start_sector, const unsigned long num_sector, unsigned char buff[]);
-    int writeSector(const int h, const int c, const int r, unsigned char buff[]);
 	void interruptHandler();
   
   protected:
@@ -36,8 +34,6 @@ class FDCDriver{
     int command(const unsigned char cmd[], const int n);
     bool wait_interrupt();
     int dma_read(const unsigned long len);
-    int dma_write(const unsigned long len);
-    void sector_to_hcr(const unsigned long sec, int& h, int& c, int& r);
     static int get_result(unsigned char r[], int n);
   
   protected:
