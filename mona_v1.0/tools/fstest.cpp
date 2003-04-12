b88a93148a91fc48bae98766071bb198b1fb8f23
Higepon (MINOWA Taro) <higepon@gmail.com>
2003-04-12 08:02:10 +0000

    interface diskdriver

--- mona_v1.0/tools/fstest.cpp
+++ mona_v1.0/tools/fstest.cpp
@@ -8,21 +8,18 @@
 int main(int argc, char *argv[]) {
 
     DiskDriver* driver = new HogeDriver("./floppy.dat", 50);
+    FAT12*      fat    = new FAT12(driver);
 
-    byte buf[512];
-    memset(buf, 0, 512);
+    if (!fat->initilize()) printf("\nfat initilze error\n");
 
-    driver->read(0, buf);
 
-    for (int i = 0; i < 512; i++) {
-        printf("%x", (byte)(buf[i]));
-    }
 
-    FAT12* fat = new FAT12(driver);
 
+
+
+
+    delete fat;
     delete driver;
-    printf("test sizeof BPB=%d. should be 59", sizeof(BPB));
 
     return 0;
-
 }
