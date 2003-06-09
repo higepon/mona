4e228239c2c1bfa62931a1b6621158ed68d8f041
Higepon (MINOWA Taro) <higepon@gmail.com>
2003-06-09 15:57:39 +0000

    add PagingUtil.cpp

---
+++ mona_v1.0/src/PagingUtil.cpp
@@ -0,0 +1,15 @@
+/*!
+    \file  PagingUtil.cpp
+    \brief class functions for set up Paging
+
+    Copyright (c) 2003 HigePon
+    WITHOUT ANY WARRANTY
+
+    \author  HigePon
+    \version $Revision$
+    \date   create:2003/06/10 update:$Date$
+*/
+
+#include<PagingUtil.h>
+
+
