8cc0142353d7c3f12be24eb5350677197a79c0db
Higepon (MINOWA Taro) <higepon@gmail.com>
2003-07-28 14:51:04 +0000

    add class Message

---
+++ mona_v1.0/src/include/Message.h
@@ -0,0 +1,26 @@
+/*!
+    \file  Message.h
+    \brief Message
+
+    class Queue & QueueMangager
+
+    Copyright (c) 2003 HigePon
+    WITHOUT ANY WARRANTY
+
+    \author  HigePon
+    \version $Revision$
+    \date   create:2003/07/28 update:$Date$
+*/
+#ifndef _MONA_MESSAGE_
+#define _MONA_MESSAGE_
+
+#include <Queue.h>
+
+class Message : public Queue {
+
+  public:
+    Message() {};
+
+};
+
+#endif
