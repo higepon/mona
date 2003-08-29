51ef9d117596280d041fcb336638cf02a828a347
Higepon (MINOWA Taro) <higepon@gmail.com>
2003-08-29 18:06:18 +0000

    add syscall user

---
+++ mona_v1.0/src/user/hello.cpp
@@ -0,0 +1,9 @@
+#include <userlib.h>
+
+void main() {
+
+    while (true) {
+
+        syscall_print("Hello Mona!!!");
+    }
+}
--- mona_v1.0/src/user/userlib.h
+++ mona_v1.0/src/user/userlib.h
@@ -4,6 +4,8 @@
 #define SYSTEM_CALL_HEAVEY        6
 #define SYSTEM_CALL_PRINT         7
 
+#define main() monamain()
+
 int syscall_sleep(dword tick);
 int syscall_heavy();
 int syscall_print(const char*);
