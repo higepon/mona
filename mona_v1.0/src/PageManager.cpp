249faf21bc7df24293f781e46492d59dc988ebe0
Higepon (MINOWA Taro) <higepon@gmail.com>
2003-08-30 04:26:02 +0000

    change licence

--- mona_v1.0/src/PageManager.cpp
+++ mona_v1.0/src/PageManager.cpp
@@ -2,8 +2,9 @@
     \file  PageManager.cpp
     \brief class PageManager
 
-    Copyright (c) 2003 HigePon
-    WITHOUT ANY WARRANTY
+    Copyright (c) 2003 Higepon
+    All rights reserved.
+    License=MIT/X Licnese
 
     \author  HigePon
     \version $Revision$
