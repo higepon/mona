f3fd5956a1ebac90545fbc97e19d4fea44be8333
Higepon (MINOWA Taro) <higepon@gmail.com>
2004-01-27 16:06:16 +0000

    user jpeg

--- mona_v1.0/src/user/hello.cpp
+++ mona_v1.0/src/user/hello.cpp
@@ -1,5 +1,6 @@
 #include <userlib.h>
 #include <KeyBoardManager.h>
+#include <jpegls.h>
 
 int listener();
 int disp();
@@ -90,12 +91,42 @@ int listener() {
     }
 }
 
+    #include "text.hex"
+
 int draw() {
 
     Screen screen;
     printf("user mode screen (x, y) = (%x, %x)\n", screen.getXResolution(), screen.getYResolution());
     printf("you can draw from user mode!! and also use multi thread \n");
 
+    CJPEGLS *jpeg = new CJPEGLS;
+
+    int w,h;
+    jpeg->Open(test_jpeg,sizeof(test_jpeg)-1);
+    jpeg->GetInfo(&w,&h);
+    byte *picture = new byte[w * h * 3];
+    jpeg->Decode(picture);
+
+
+    int x,y;
+
+    int vesaWidth = screen.getXResolution();
+    int vesaBpp = screen.getBpp();
+    byte* vesaVram = screen.getVRAM();
+
+    for(y=0;y<h;y++){
+        for(x=0;x<w;x++){
+            int k =(x+(y*vesaWidth))*vesaBpp/8;
+            int k2=(x+(y*w))*3;
+            /*                picture[k2]=0xFF;
+                              picture[k2+1]=0xFF;
+                              picture[k2+2]=0xFF;*/
+            vesaVram[k]   = picture[k2];
+            vesaVram[k+1] = picture[k2+1];
+            vesaVram[k+2] = picture[k2+2];
+        }
+    }
+    for (;;);
     word color = Color::bpp24to565(Color::rgb(0x7A, 0x7A, 0xC4));
 
     screen.fillRect16(0, 200, 800, 400, color);
