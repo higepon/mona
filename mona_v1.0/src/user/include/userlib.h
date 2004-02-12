45cda213394b007191c74ce257092cd121938d1b
Higepon (MINOWA Taro) <higepon@gmail.com>
2004-02-12 14:31:12 +0000

    Observers/Observable

--- mona_v1.0/src/user/include/userlib.h
+++ mona_v1.0/src/user/include/userlib.h
@@ -107,6 +107,38 @@ class MonaApplication;
 extern MonaApplication* monaApp;
 
 /*----------------------------------------------------------------------
+    Observable/Observer
+----------------------------------------------------------------------*/
+class Observable;
+interface Observer;
+interface Observer {
+
+  public:
+    virtual void update(Observable* o, void* arg) = 0;
+};
+
+class Observable {
+
+  public:
+    Observable() : changed(false) {}
+    virtual ~Observable(){}
+
+  public:
+    virtual void addObserver(Observer* o);
+    virtual void notifyObservers();
+    virtual void notifyObservers(void* arg);
+    virtual void deleteObservers();
+    virtual void setChanged();
+    virtual void clearChanged();
+    virtual bool hasChanged() const;
+    virtual int countObservers();
+
+  protected:
+    HList<Observer*> observers;
+    bool changed;
+};
+
+/*----------------------------------------------------------------------
     Date
 ----------------------------------------------------------------------*/
 class Date {
