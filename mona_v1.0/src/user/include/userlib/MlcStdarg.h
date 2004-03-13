864e6420f2e153f3de84076b84958ef6e283c5d6
shadowin <none>
2004-03-13 03:46:02 +0000

    Create MlcStdarg.h

---
+++ mona_v1.0/src/user/include/userlib/MlcStdarg.h
@@ -0,0 +1,41 @@
+/*!
+  \file   MlcStdarg.h
+  \brief  mona c standard argument
+
+  Copyright (c) 2002-2004 shadow
+  All rights reserved.<BR>
+  \b License NYSL<BR>
+  \b Create 2004/03/13
+  \author  shadow
+
+  $Revision$
+  $Date$
+*/
+#ifndef _MONA_LIB_STDARG_
+#define _MONA_LIB_STDARG_
+
+#include <types.h>
+
+#ifndef __SIZE_TYPE__
+#define __SIZE_TYPE__ unsigned long
+#endif
+
+typedef __SIZE_TYPE__ size_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef char *va_list;
+#define va_start(ap,last) (void)((ap)=(va_list)&(last)+sizeof(last))
+#define va_arg(ap,type) (*((type*)(ap))++)
+#define va_end(ap) (void)((ap)=NULL)
+
+int vsprintf(char *s, const char *format, va_list arg);
+int vsscanf(const char *s, const char *format, va_list arg);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
