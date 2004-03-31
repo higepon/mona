d161bbdf83c806a425387f22e88c26d4648ccc6b
Higepon (MINOWA Taro) <higepon@gmail.com>
2004-03-31 14:13:00 +0000

    jpegdeo.elf

--- mona_v1.0/src/user/jpegls/jpegdemo.cpp
+++ mona_v1.0/src/user/jpegls/jpegdemo.cpp
@@ -1,11 +1,17 @@
 #include <userlib.h>
 #include <jpegls.h>
 
-#include "text.hex"
+//#include "text.hex"
 int MonaMain(List<char*>* pekoe) {
 
+    if (pekoe->size() != 1)
+    {
+        printf("usage: JPEGDEMO.ELF XXXX.JPG\n");
+        return -1;
+    }
+
     Screen screen;
-    printf("user mode screen (x, y) = (%x, %x) %dbpp\n", screen.getWidth(), screen.getHeight(), screen.getBpp());
+    printf("user mode screen (x, y) = (%d, %d) %dbpp\n", screen.getWidth(), screen.getHeight(), screen.getBpp());
 
     /* check bpp */
     if (screen.getBpp() < 24) {
@@ -18,8 +24,39 @@ int MonaMain(List<char*>* pekoe) {
     /* nikq code */
     CJPEGLS *jpeg = new CJPEGLS;
 
+    FileInputStream fis(pekoe->get(0));
+
+    if (fis.open() != 0)
+    {
+        printf("file %s not found", pekoe->get(0));
+        return -1;
+    }
+
+    int size = fis.getFileSize();
+
+    byte* buf = (byte*)malloc(size);
+    if (buf == NULL)
+    {
+        printf("memory allocate error\n");
+        return -1;
+    }
+
+    if (fis.read(buf, size) != 0)
+    {
+        printf("file read error\n");
+        return -1;
+    }
+
+    fis.close();
+
+    if (jpeg->Open(buf, size) != 0)
+    {
+        printf("not supported format image\n");
+        free(buf);
+        return -1;
+    }
+
     int w,h;
-    jpeg->Open(test_jpeg,sizeof(test_jpeg)-1);
     jpeg->GetInfo(&w,&h);
     byte *picture = new byte[w * h * 3];
     jpeg->Decode(picture);
@@ -36,11 +73,12 @@ int MonaMain(List<char*>* pekoe) {
             /*                picture[k2]=0xFF;
                               picture[k2+1]=0xFF;
                               picture[k2+2]=0xFF;*/
-            vesaVram[k]   = picture[k2];
+            vesaVram[k]   = picture[k2+2];
             vesaVram[k+1] = picture[k2+1];
-            vesaVram[k+2] = picture[k2+2];
+            vesaVram[k+2] = picture[k2+0];
         }
     }
 
+    free(buf);
     return 0;
 }
