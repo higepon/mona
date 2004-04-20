f948709a51b2370174363b57f1abadddfb49f145
tino <none>
2004-04-20 20:54:35 +0000

    Modified Files: 	Mona/samples/gui/bitmap/Form1.cpp

--- Mona/samples/gui/bitmap/Form1.cpp
+++ Mona/samples/gui/bitmap/Form1.cpp
@@ -1,8 +1,8 @@
 // This file is in the public domain.
 // There are no restrictions on any sort of usage of this file.
 
-#include <gui/System/Mona/Forms/Application.h>
 #include <gui/System/Console.h>
+#include <gui/System/Mona/Forms/Application.h>
 #include <gui/System/Mona/Forms/Form.h>
 #include <gui/System/Mona/Forms/Label.h>
 
