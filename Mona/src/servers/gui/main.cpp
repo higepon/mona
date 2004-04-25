07269dc15c25f9df623adf92863cffefa21eb574
tino <none>
2004-04-25 09:55:41 +0000

    Modified Files: 	ChangeLog bin/root/MONA.CFG src/servers/gui/main.cpp

--- Mona/src/servers/gui/main.cpp
+++ Mona/src/servers/gui/main.cpp
@@ -1,6 +1,8 @@
 // This software is in the public domain.
 // There are no restrictions on any sort of usage of this software.
 
+// This file's encoding is UTF-8.
+
 #include <monapi.h>
 #include <monalibc.h>
 #include <jpegls.h>
@@ -25,6 +27,17 @@ static int SendMessage(dword to, dword header, dword arg1, dword arg2, dword arg
 	MessageInfo msg;
 	Message::create(&msg, header, arg1, arg2, arg3, (char*)str);
 	return Message::send(to, &msg);
+}
+
+MessageInfo WaitMessage(dword from, dword header, dword arg1)
+{
+	MessageInfo msg;
+	for (;;)
+	{
+		if (MonAPI::Message::receive(&msg)) continue;
+		if (msg.from == from && msg.header == header && msg.arg1 == arg1) break;
+	}
+	return msg;
 }
 
 class MemoryInfo
@@ -68,11 +81,14 @@ public:
 
 static MemoryInfo* default_font = NULL;
 static MemoryInfo* wallpaper = NULL;
-static int wallpaper_pos = 0;
+static int wallpaper_pos = 0, wallpaper_transparent = -1;
 static bool wallpaper_prompt = false;
+static Screen* screen = NULL;
+dword msvr = 0;
 
 static MemoryInfo* ReadFile(const char* file, bool prompt = false)
 {
+	if (prompt) printf("%s: Reading %s....", SVR, file);
 	FileInputStream fis((char*)file);
 	if (fis.open() != 0)
 	{
@@ -95,7 +111,6 @@ static MemoryInfo* ReadFile(const char* file, bool prompt = false)
 
 static void ReadFont(const char* file)
 {
-	printf("%s: Reading %s....", SVR, file);
 	MemoryInfo* mi = ReadFile(file, true);
 	if (mi == NULL) return;
 	
@@ -174,9 +189,9 @@ static MemoryInfo* ReadJPEG(MemoryInfo* mi)
 	return ret;
 }
 
-static MemoryInfo* ReadImage(const char* file)
+static MemoryInfo* ReadImage(const char* file, bool prompt = false)
 {
-	MemoryInfo* mi = ReadFile(file), * ret = NULL;
+	MemoryInfo* mi = ReadFile(file, prompt), * ret = NULL;
 	if (mi == NULL) return ret;
 	
 	const char* p = file;
@@ -196,13 +211,172 @@ static MemoryInfo* ReadImage(const char* file)
 	return ret;
 }
 
+void SetDefaultScreen()
+{
+	if (screen == NULL) screen = new Screen();
+}
+
+void SetMouseCursor(bool enabled)
+{
+	static bool first = true;
+	if (first)
+	{
+		msvr = Message::lookupMainThread("MOUSE.SVR");
+		if (msvr == 0xFFFFFFFF)
+		{
+			printf("%s: Can't connect to mouse server!\n", SVR);
+		}
+		first = false;
+	}
+	if (msvr == 0xFFFFFFFF) return;
+	
+	dword hdr = enabled ? MSG_MOUSE_ENABLE_CURSOR : MSG_MOUSE_DISABLE_CURSOR;
+	SendMessage(msvr, hdr, 0, 0, 0);
+	WaitMessage(msvr, MSG_RESULT_OK, hdr);
+}
+
+void DrawImage(MemoryInfo* img, int spx, int spy, int transparent = -1)
+{
+	SetDefaultScreen();
+	SetMouseCursor(false);
+	unsigned char* vram = screen->getVRAM();
+	int bpp = screen->getBpp(), sw = screen->getWidth(), sh = screen->getHeight();
+	int bypp = bpp >> 3;
+	byte* pBuf = img->Data;
+	for (int y = 0; y < img->Height; y++)
+	{
+		int sy = spy + y;
+		if (sy < 0 || sh <= sy)
+		{
+			pBuf += img->Width * 4;
+			continue;
+		}
+		unsigned char* pVram = &vram[(spx + sy * sw) * bypp];
+		for (int x = 0; x < img->Width; x++, pVram += bypp, pBuf += 4)
+		{
+			int sx = spx + x, c32 = (int)((*(unsigned int*)pBuf) & 0xffffff);
+			if (sx < 0 || sw <= sx || pBuf[3] == 0 || c32 == transparent) continue;
+			
+			switch (bpp)
+			{
+				case 8: // broken
+				{
+					byte c = (pBuf[0] + pBuf[1] + pBuf[2]) / 3;
+					if (*pVram != c) *pVram = c;
+					break;
+				}
+				case 16: // broken
+				{
+					unsigned short c = pBuf[0] + (pBuf[1] << 8);
+					if (*(unsigned short*)pVram != c) *(unsigned short*)pVram = c;
+					break;
+				}
+				case 24:
+					if (pVram[0] != pBuf[0]) pVram[0] = pBuf[0];
+					if (pVram[1] != pBuf[1]) pVram[1] = pBuf[1];
+					if (pVram[2] != pBuf[2]) pVram[2] = pBuf[2];
+					break;
+				case 32:
+					if (*(int*)pVram != c32) *(int*)pVram = c32;
+					break;
+			}
+		}
+	}
+	SetMouseCursor(true);
+}
+
+void DrawWallPaper()
+{
+	if (wallpaper == NULL) return;
+	
+	SetDefaultScreen();
+	int x = 0, y = 0, sw = screen->getWidth(), sh = screen->getHeight();
+	switch (wallpaper_pos)
+	{
+		case 1: // ¶ã
+			break;
+		case 2: // ’†‰›ã
+			x = (sw - wallpaper->Width) / 2;
+			break;
+		case 3: // ‰Eã
+			x = sw - wallpaper->Width;
+			break;
+		case 4: // ¶’†‰›
+			y = (sh - wallpaper->Height) / 2;
+			break;
+		case 5: // ’†‰›
+			x = (sw - wallpaper->Width) / 2;
+			y = (sh - wallpaper->Height) / 2;
+			break;
+		case 6: // ‰E’†‰›
+			x = sw - wallpaper->Width;
+			y = (sh - wallpaper->Height) / 2;
+			break;
+		case 7: // ¶‰º
+			y = sh - wallpaper->Height;
+			break;
+		case 8: // ’†‰›‰º
+			x = (sw - wallpaper->Width) / 2;
+			y = sh - wallpaper->Height;
+			break;
+		case 9: // ‰E‰º
+			x = sw - wallpaper->Width;
+			y = sh - wallpaper->Height;
+			break;
+	}
+	DrawImage(wallpaper, x, y, wallpaper_transparent);
+}
+
+void DrawWallPaper(const char* src, int pos, int transparent)
+{
+	if (wallpaper != NULL) wallpaper->Dispose();
+	wallpaper = ReadImage(src, wallpaper_prompt);
+	wallpaper_prompt = false;
+	if (wallpaper == NULL) return;
+	
+	wallpaper_pos = pos;
+	wallpaper_transparent = transparent;
+	DrawWallPaper();
+}
+
+int xtoi(const char* str)
+{
+	if (str == NULL) return 0;
+	
+	int ret = 0;
+	int len = strlen(str);
+	for (int i = len - 1, sh = 0; i >= 0; i--, sh += 4)
+	{
+		char ch = str[i];
+		int v = 0;
+		if ('0' <= ch && ch <= '9')
+		{
+			v = ch - '0';
+		}
+		else if ('A' <= ch && ch <= 'F')
+		{
+			v = ch - 'A' + 10;
+		}
+		else if ('a' <= ch && ch <= 'f')
+		{
+			v = ch - 'a' + 10;
+		}
+		else
+		{
+			return 0;
+		}
+		ret += v << sh;
+	}
+	return ret;
+}
+
 void ReadConfig()
 {
 	MemoryInfo* cfg = ReadFile("/MONA.CFG");
 	if (cfg == NULL) return;
 	
     char line[256], src[256] = "";
-    int linepos = 0, wppos = 5;
+    int linepos = 0, wppos = 5, wptp = -1;
     for (int pos = 0; pos <= cfg->Size; pos++)
     {
         char ch = pos < cfg->Size ? (char)cfg->Data[pos] : '\n';
@@ -213,9 +387,13 @@ void ReadConfig()
                 {
                 	strcpy(src, &line[17]);
                 }
-                if (strstr(line, "WALLPAPER_POSITION=") == line)
+                else if (strstr(line, "WALLPAPER_POSITION=") == line)
                 {
                 	wppos = atoi(&line[19]);
+                }
+                else if (strstr(line, "WALLPAPER_TRANSPARENT=") == line)
+                {
+                	wptp = xtoi(&line[22]);
                 }
                 linepos = 0;
             }
@@ -228,7 +406,7 @@ void ReadConfig()
     if (src[0] == '\0') return;
     
     wallpaper_prompt = true;
-    SendMessage(System::getThreadID(), MSG_GUISERVER_SETWALLPAPER, wppos, 0, 0, src);
+    DrawWallPaper(src, wppos, wptp);
 }
 
 int MonaMain(List<char*>* pekoe)
@@ -272,11 +450,23 @@ int MonaMain(List<char*>* pekoe)
 				MemoryMap::unmap(msg.arg1);
 				break;
 			case MSG_GUISERVER_SETWALLPAPER:
+				DrawWallPaper(msg.str, msg.arg1, msg.arg2);
+				SendMessage(msg.from, MSG_RESULT_OK, msg.header, 0, 0);
+				break;
+			case MSG_GUISERVER_REFRESHWALLPAPER:
+				DrawWallPaper();
+				SendMessage(msg.from, MSG_RESULT_OK, msg.header, 0, 0);
 				break;
 		}
 	}
 	
 	default_font->Dispose();
 	delete default_font;
+	if (wallpaper != NULL)
+	{
+		wallpaper->Dispose();
+		delete wallpaper;
+	}
+	if (screen != NULL) delete screen;
 	return 0;
 }
