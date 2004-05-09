6c63642d26d4dac476278a0802cc41b1e109198a
tino <none>
2004-05-09 23:22:58 +0000

    Modified Files: 	apps/gui/shell/Form1.cpp

--- Mona/apps/gui/shell/Form1.cpp
+++ Mona/apps/gui/shell/Form1.cpp
@@ -11,15 +11,58 @@ using namespace System::Mona::Forms;
 class Terminal : public Control
 {
 public:
-	Terminal() : x(0), y(0) {}
+	Terminal() : x(0), y(0)
+	{
+		this->set_BackColor(Color::get_White());
+	}
+	
 	virtual ~Terminal() {}
 
 	void Output(String s)
 	{
 		_P<Graphics> g = this->CreateGraphics();
-		Size sz = g->MeasureString(s, Control::get_DefaultFont());
-		g->DrawString(s, Control::get_DefaultFont(), Color::get_Black(), x, y);
-		y += sz.Height;
+		String ss;
+		int w = this->get_Width(), h = this->get_Height();
+		wchar prev = 0;
+		FOREACH (wchar, ch, s)
+		{
+			if (ch == '\r' || ch == '\n')
+			{
+				if (prev == '\r' && ch == '\n') continue;
+				if (ss.get_Length() > 0)
+				{
+					g->DrawString(ss, Control::get_DefaultFont(), Color::get_Black(), x, y);
+					ss.Unset();
+				}
+				x = 0;
+				if (y < h - 24)
+				{
+					y += 12;
+				}
+				else
+				{
+					Color* img = this->buffer->get();
+					int len = (h - 12) * w, d = w * 12;
+					for (int i = 0; i < len; i++)
+					{
+						img[i] = img[i + d];
+					}
+					x = 0;
+					g->FillRectangle(this->get_BackColor(), 0, y, w, h - y);
+				}
+			}
+			else
+			{
+				ss += ch;
+			}
+			prev = ch;
+		}
+		if (ss.get_Length() > 0)
+		{
+			Size sz = g->MeasureString(ss, Control::get_DefaultFont());
+			g->DrawString(ss, Control::get_DefaultFont(), Color::get_Black(), x, y);
+			x += sz.Width;
+		}
 		g->Dispose();
 	}
 
@@ -77,7 +120,7 @@ public:
 		this->InitializeComponent();
 		
 		terminal = new Terminal();
-		terminal->set_Bounds(Rectangle(0, 0, 256, 160));
+		terminal->set_Bounds(Rectangle(0, 0, 400, 256));
 		this->get_Controls()->Add(terminal.get());
 	}
 	
@@ -85,7 +128,7 @@ private:
 	void InitializeComponent()
 	{
 		this->set_Location(Point(128, 128));
-		this->set_ClientSize(Size(256, 160));
+		this->set_ClientSize(Size(400, 256));
 		this->set_Text("GUI Shell");
 	}
 	
