1a1e7bdc8bf8b47640fed306df9170ddb535ce06
Higepon (MINOWA Taro) <higepon@gmail.com>
2004-08-28 11:44:09 +0000

    hige3d

--- Mona/apps/gui/hige3D/Form1.cpp
+++ Mona/apps/gui/hige3D/Form1.cpp
@@ -1,5 +1,7 @@
 #include <gui/System/Mona/Forms/Application.h>
 #include <gui/System/Mona/Forms/Form.h>
+#include <monalibc/math.h>
+#include <gui/System/Mona/Forms/Timer.h>
 
 #define SCREEN_W 256
 #define SCREEN_H 256
@@ -46,8 +48,6 @@ class Canvas : public Control
 public:
     Canvas()
     {
-        this->set_BackColor(Color::get_White());
-
         this->CreateModel();
     }
 
@@ -56,58 +56,113 @@ public:
         this->DeleteModel();
     }
 
+protected:
+    virtual void OnMouseMove(_P<MouseEventArgs> e)
+    {
+        if (this->drag)
+        {
+            int dx = e->X - this->prevX;
+            int dy = e->Y - this->prevY;
+
+            double phi   =  dx * this->sense;
+            double theta =  dy * this->sense;
+
+            this->Rotate(phi,theta);
+            this->Refresh();
+
+            this->prevX = e->X;
+            this->prevY = e->Y;
+        }
+        Control::OnMouseMove(e);
+    }
+
+    virtual void OnMouseDown(_P<MouseEventArgs> e)
+    {
+        this->drag = true;
+        prevX = e->X;
+        prevY = e->Y;
+        Control::OnMouseDown(e);
+    }
+
+    virtual void OnMouseUp(_P<MouseEventArgs> e)
+    {
+        this->drag = false;
+        Control::OnMouseUp(e);
+    }
+
+    virtual void OnMouseLeave(_P<EventArgs> e)
+    {
+        this->drag = false;
+        Control::OnMouseLeave(e);
+    }
+
 public:
     void OnPaint()
     {
         _P<Graphics> g = this->CreateGraphics();
 
+        g->FillRectangle(Color::get_White(), 0, 0, SCREEN_W, SCREEN_H);
         this->DrawModel(g);
 
         g->Dispose();
     }
 
-protected:
-
-    void DrawModel(_P<Graphics> g)
+    void Rotate(double phi, double theta)
     {
-        for (int i = 0; i < faceNum; i++)
+        double tx, ty, tz;
+        double tmpSin = sin(theta);
+        double tmpCos = cos(theta);
+
+        for (int i = 0; i < vertexNum; i++)
         {
-            int x0 = (int)(face[i]->vertex[0]->x * this->scale + CENTER_X);
-            int x1 = (int)(face[i]->vertex[1]->x * this->scale + CENTER_X);
-            int x2 = (int)(face[i]->vertex[2]->x * this->scale + CENTER_X);
-            int y0 = (int)(face[i]->vertex[0]->y * this->scale + CENTER_Y);
-            int y1 = (int)(face[i]->vertex[1]->y * this->scale + CENTER_Y);
-            int y2 = (int)(face[i]->vertex[2]->y * this->scale + CENTER_Y);
-            g->DrawLine(Color::get_Blue(), x0, y0, x1, y1);
-            g->DrawLine(Color::get_Blue(), x1, y1, x2, y2);
-            g->DrawLine(Color::get_Blue(), x2, y2, x0, y0);
+            ty = vertex[i]->y * tmpCos + vertex[i]->z * tmpSin;
+            tz = -vertex[i]->y * tmpSin + vertex[i]->z * tmpCos;
+            vertex[i]->y = ty;
+            vertex[i]->z = tz;
 
         }
+
+        tmpSin = sin(phi);
+        tmpCos = cos(phi);
+
+        for (int i = 0; i < vertexNum; i++)
+        {
+            tx = vertex[i]->x * tmpCos - vertex[i]->z * tmpSin;
+            tz = vertex[i]->x * tmpSin + vertex[i]->z * tmpCos;
+            vertex[i]->x = tx;
+            vertex[i]->z = tz;
+        }
     }
+
+protected:
 
     void CreateModel()
     {
         this->vertexNum = 6;
-        this->faceNum = 8;
+        this->faceNum   = 8;
+        this->sense     = 0.01;
+
+        this->prevX = CENTER_X;
+        this->prevY = CENTER_Y;
 
         this->vertex = new Vertex*[this->vertexNum];
         this->face = new Face*[this->faceNum];
 
-        vertex[0]=new Vertex(-1, 0, 0);
-        vertex[1]=new Vertex( 0, 0, 1);
-        vertex[2]=new Vertex( 0, 1, 0);
-        vertex[3]=new Vertex( 1, 0, 0);
-        vertex[4]=new Vertex( 0,-1, 0);
-        vertex[5]=new Vertex( 0, 0,-1);
+        vertex[0] = new Vertex(-1, 0, 0);
+        vertex[1] = new Vertex( 0, 0, 1);
+        vertex[2] = new Vertex( 0, 1, 0);
+        vertex[3] = new Vertex( 1, 0, 0);
+        vertex[4] = new Vertex( 0,-1, 0);
+        vertex[5] = new Vertex( 0, 0,-1);
 
-        face[0]=new Face( vertex[1], vertex[4], vertex[2]);
-        face[1]=new Face( vertex[1], vertex[0], vertex[4]);
-        face[2]=new Face( vertex[1], vertex[2], vertex[0]);
-        face[3]=new Face( vertex[3], vertex[2], vertex[4]);
-        face[4]=new Face( vertex[0], vertex[5], vertex[4]);
-        face[5]=new Face( vertex[4], vertex[5], vertex[3]);
-        face[6]=new Face( vertex[3], vertex[5], vertex[2]);
-        face[7]=new Face( vertex[2], vertex[5], vertex[0]);
+        face[0] = new Face( vertex[1], vertex[4], vertex[2]);
+        face[1] = new Face( vertex[1], vertex[0], vertex[4]);
+        face[2] = new Face( vertex[1], vertex[2], vertex[0]);
+        face[3] = new Face( vertex[3], vertex[2], vertex[4]);
+        face[4] = new Face( vertex[0], vertex[5], vertex[4]);
+        face[5] = new Face( vertex[4], vertex[5], vertex[3]);
+        face[6] = new Face( vertex[3], vertex[5], vertex[2]);
+        face[7] = new Face( vertex[2], vertex[5], vertex[0]);
 
         this->scale = 100;
     }
@@ -128,10 +183,31 @@ protected:
         delete[] this->face;
     }
 
+    void DrawModel(_P<Graphics> g)
+    {
+        for (int i = 0; i < faceNum; i++)
+        {
+            int x0 = (int)(face[i]->vertex[0]->x * this->scale + CENTER_X);
+            int x1 = (int)(face[i]->vertex[1]->x * this->scale + CENTER_X);
+            int x2 = (int)(face[i]->vertex[2]->x * this->scale + CENTER_X);
+            int y0 = (int)(face[i]->vertex[0]->y * this->scale + CENTER_Y);
+            int y1 = (int)(face[i]->vertex[1]->y * this->scale + CENTER_Y);
+            int y2 = (int)(face[i]->vertex[2]->y * this->scale + CENTER_Y);
+            g->DrawLine(Color::get_Blue(), x0, y0, x1, y1);
+            g->DrawLine(Color::get_Blue(), x1, y1, x2, y2);
+            g->DrawLine(Color::get_Blue(), x2, y2, x0, y0);
+
+        }
+    }
+
 protected:
+    bool drag;
+    int prevX;
+    int prevY;
     int vertexNum;
     int faceNum;
     double scale;
+    double sense;
     Vertex** vertex;
     Face** face;
 };
