7ea49bfb970c87f298ac118d1c8cbc7c69c6a9bf
Higepon (MINOWA Taro) <higepon@gmail.com>
2004-08-30 15:54:33 +0000

    3d

--- Mona/apps/gui/hige3D/Form1.cpp
+++ Mona/apps/gui/hige3D/Form1.cpp
@@ -96,6 +96,134 @@ protected:
         Control::OnMouseLeave(e);
     }
 
+    virtual void FillFace(_P<Graphics> g, Face* face, Color color)
+    {
+        int maxX;
+        int midX;
+        int minX;
+        Vertex* v1;
+        Vertex* v2;
+        Vertex* v3;
+	double x1, x2, x3;
+	double y1, y2, y3;
+
+        GetRelativePostion(face, &maxX, &midX, &minX);
+        v1 = face->vertex[minX];
+        v2 = face->vertex[midX];
+        v3 = face->vertex[maxX];
+
+	x1 = v1->x * scale;
+	y1 = v1->y * scale;
+	x2 = v2->x * scale;
+	y2 = v2->y * scale;
+	x3 = v3->x * scale;
+	y3 = v3->y * scale;
+
+	double dx1 = x2 - x1;
+	double dx2 = x3 - x1;
+	double dx3 = x3 - x2;
+
+	if (dx1 == 0.0f || dx2 == 0.0f) return;
+
+	double a1 = (y2 - y1) / dx1;
+	double a2 = (y3 - y1) / dx2;
+
+	for (int x = (int)x1; x < (int)x2; x++)
+	{
+	    int yy1 = (int)(a1 * (x - x1) + y1);
+	    int yy2 = (int)(a2 * (x - x1) + y1);
+	    int s = yy1 > yy2 ? -1 : 1;
+
+            g->DrawLine(color, x + CENTER_X, yy1 + CENTER_Y + s, x + CENTER_X, yy2 + CENTER_Y - s);
+	}
+
+	if (dx3 == 0.0f) return;
+
+	double a3 = (y3 - y2) / dx3;
+
+	for (int x = (int)x2; x < (int)x3; x++)
+	{
+	    int yy1 = (int)(a2 * (x - x3) + y3);
+	    int yy2 = (int)(a3 * (x - x3) + y3);
+	    int s = yy1 > yy2 ? -1 : 1;
+
+            g->DrawLine(color, x + CENTER_X, yy1 + CENTER_Y + s, x + CENTER_X, yy2 + CENTER_Y - s);
+	}
+
+
+
+
+// 	if ((v2->x - v1->x) == 0 ||  (v3->x - v1->x) == 0) return;
+
+//         double a1 = (v2->y - v1->y) / (v2->x - v1->x);
+//         double a2 = (v3->y - v1->y) / (v3->x - v1->x);
+
+//         for (int x = (int)(v1->x * scale); x < (int)(v2->x * scale); x++)
+//         {
+//             int y1 = (int)(a1 * (x - v1->x) * scale + v1->y * scale);
+//             int y2 = (int)(a2 * (x - v1->x) * scale + v1->y * scale);
+
+// 	char buf[512];
+// 	sprintf(buf, "(%d,%d,%d,%d)<%d, %d>", x, y1, x, y2, (int)(a1 * (x - v1->x) * scale), (int)(a2 * (x - v1->x) * scale));
+// 	sprintf(buf, "[%1.99f, %1.99f]", v2->x, v1->x);
+// 	syscall_print(buf);
+
+//            g->DrawLine(color, x + CENTER_X, y1 + CENTER_Y, x + CENTER_X, y2 + CENTER_Y);
+	//      }
+    }
+
+    virtual void GetRelativePostion(Face* face, int* maxX, int* midX, int* minX)
+    {
+        double x[3];
+        x[0] = face->vertex[0]->x;
+        x[1] = face->vertex[1]->x;
+        x[2] = face->vertex[2]->x;
+
+        if (x[0] >= x[1] && x[0] >= x[2])
+        {
+            *maxX = 0;
+
+            if (x[1] >= x[2])
+            {
+                *midX = 1;
+                *minX = 2;
+            }
+            else
+            {
+                *midX = 2;
+                *minX = 1;
+            }
+        }
+        else if (x[0] >= x[1] && x[0] < x[2])
+        {
+            *maxX = 2;
+            *midX = 0;
+            *minX = 1;
+
+        }
+        else if (x[0] < x[1] && x[0] >= x[2])
+        {
+            *maxX = 1;
+            *midX = 0;
+            *minX = 2;
+        }
+        else
+       {
+           *minX = 0;
+
+           if (x[1] >= x[2])
+           {
+               *maxX = 1;
+               *midX = 2;
+           }
+           else
+           {
+               *maxX = 2;
+               *midX = 1;
+           }
+       }
+    }
+
 public:
     void OnPaint()
     {
@@ -196,7 +324,7 @@ protected:
             g->DrawLine(Color::get_Blue(), x0, y0, x1, y1);
             g->DrawLine(Color::get_Blue(), x1, y1, x2, y2);
             g->DrawLine(Color::get_Blue(), x2, y2, x0, y0);
-
+            FillFace(g, face[i], Color::get_Yellow());
         }
     }
 
