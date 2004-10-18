fee54823499a8ca12a307d3bd3eeaf96a37724ba
bayside <none>
2004-10-18 16:35:27 +0000

    ������������������������ ������������������

---
+++ Mona/src/lib/baygui/String.cpp
@@ -0,0 +1,70 @@
+/*
+Copyright (c) 2004 bayside
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "baygui.h"
+
+/** コンストラクタ */
+String::String(char *s)
+{
+	c_str = s;
+}
+
+/** デストラクタ */
+String::~String()
+{
+	free(c_str);
+}
+
+/** 文字列の長さを返す */
+int String::length()
+{
+	return strlen(c_str);
+}
+
+/** 文字列を返す */
+char *String::toString()
+{
+	return c_str;
+}
+
+/** 指定した文字列で始まっているかどうか判定する */
+bool String::startsWith(String *s)
+{
+	return false;
+}
+
+/** 指定した文字列で終わっているかどうか判定する */
+bool endsWidth(String *s)
+{
+	return false;
+}
+
+/** 指定した文字列と等しいかどうか判定する */
+bool String::equals(String *s)
+{
+	return false;
+}
