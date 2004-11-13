df8f4c871eeeeaacd06759da09a8b1bb151704d2
yamamimail <none>
2004-11-13 17:32:20 +0000

    no message

--- Mona/samples/non-gui/yamami/main.cpp
+++ Mona/samples/non-gui/yamami/main.cpp
@@ -1,10 +1,27 @@
+/*!
+    \file   main.cpp
+    \brief  Yamami Code:Mones リリース前 各種テストロジック
+
+    Copyright (c) 2004 Yamami
+    All rights reserved.
+    License=MIT/X License
+
+    \author  Yamami
+    \version $Revision$
+    \date   create:2004/08/28 update:$Date$
+*/
+
 #include <sys/types.h>
 #include <monapi.h>
 #include <pci/Pci.h>
+#include <sys/HashMap.h>
+
+//共有メモリ使用
+#include <monapi/cmemoryinfo.h>
 
 using namespace MonAPI;
 
-#define MAIN_2
+#define MAIN_5
 
 
 #ifdef MAIN_1
@@ -75,6 +92,218 @@ int MonaMain(List<char*>* pekoe)
     else{
         printf("Device Not Exist!! \n");
     }
+    
+    return 0;
+
+}
+
+#endif
+
+
+#ifdef MAIN_3
+
+
+struct REPLY_WAIT{
+    int ip;         /* Request IP address. */
+    int repFlg;         /* Reply flag. */
+    int wait;
+    char mac[6];        /* Reply mac buffer. */
+};
+
+//HLIST使用テスト
+int MonaMain(List<char*>* pekoe)
+{
+    List<REPLY_WAIT*>* testList;
+    REPLY_WAIT* addWork;
+    
+    testList = new HList<REPLY_WAIT*>();
+    
+    
+    for (int i = 0; i < 3; i++) {
+        addWork = new REPLY_WAIT();
+        addWork->ip = i;
+        addWork->wait = i;
+        sprintf(addWork->mac , "%d",i*2);
+        
+        //ここで、HListへ追加
+        testList->add(addWork);
+    }
+    
+    for (int i = 0; i < testList->size() ; i++) {
+
+        REPLY_WAIT* p = testList->get(i);
+        
+        printf("ip=%d wait=%d mac=%s \n", p->ip , p->wait , p->mac);
+        
+    }
+    
+    
+    
+    return 0;
+
+}
+
+
+#endif
+
+
+#ifdef MAIN_4
+
+
+//Yamami 自己メッセージ通知テスト
+int MonaMain(List<char*>* pekoe)
+{
+
+    MessageInfo info;
+
+    // テストサーバのIDを見つける
+    dword targetID = Message::lookupMainThread("YAMAS.EX2");
+    if (targetID == 0xFFFFFFFF)
+    {
+        printf("local!!!! yamas:INIT not found\n");
+        exit(1);
+    }
+
+    // create message
+    Message::create(&info, MSG_YAMATEST, 0, 0, 0, NULL);
+    // send
+    if (Message::send(targetID, &info)) {
+        printf("local!!!! yamas:INIT error\n");
+    }
+
+    
+    return 0;
+
+}
+
+#endif
+
+
+
+#ifdef MAIN_5
+
+
+struct REPLY_WAIT{
+    int ip;         /* Request IP address. */
+    int repFlg;         /* Reply flag. */
+    int wait;
+    char mac[6];        /* Reply mac buffer. */
+};
+
+//HashMap使用テスト
+int MonaMain(List<char*>* pekoe)
+{
+    HashMap<REPLY_WAIT*>* testHash;
+    
+    REPLY_WAIT* addWork;
+    char IpKey[10];    //
+
+    
+    testHash = new HashMap<REPLY_WAIT*>(3);
+    
+    //for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 5; i++) {
+        //要素数を超えるとどうなる？？
+        addWork = new REPLY_WAIT();
+        addWork->ip = i;
+        addWork->wait = i;
+        sprintf(addWork->mac , "%d",i*2);
+        
+        //IPアドレスを、MAPのキー化(String化)
+        sprintf(IpKey , "%08x",i);
+        
+        //ここで、HashMapへ追加
+        testHash->put(IpKey , addWork);
+        
+    }
+    
+    //for (int i = 0; i < testHash->size() ; i++) {
+    for (int i = 0; i < 5 ; i++) {
+
+        //IPアドレスを、MAPのキー化(String化)
+        sprintf(IpKey , "%08x",i);
+
+        REPLY_WAIT* p = testHash->get(IpKey);
+        
+        printf("ip=%d wait=%d mac=%s \n", p->ip , p->wait , p->mac);
+        
+    }
+    
+    return 0;
+
+}
+
+#endif
+
+
+
+#ifdef MAIN_6
+
+struct REPLY_WAIT{
+    int ip;         /* Request IP address. */
+    int repFlg;         /* Reply flag. */
+    int wait;
+    char mac[6];        /* Reply mac buffer. */
+};
+
+//共有メモリテスト
+int MonaMain(List<char*>* pekoe)
+{
+    
+    //まず、monapi_cmemoryinfo構造体をnew
+    monapi_cmemoryinfo* cmInfo = new monapi_cmemoryinfo();
+    
+    REPLY_WAIT* addWork;
+    
+    if (!monapi_cmemoryinfo_create(cmInfo, (dword)(sizeof(REPLY_WAIT) + 1), 0))
+    {
+        monapi_cmemoryinfo_delete(cmInfo);
+        printf("monapi_cmemoryinfo_create error\n");
+    }
+    
+    //この時点で何がセットされている？
+    logprintf("monapi_cmemoryinfo_create after!!!\n");
+    
+    logprintf("cmInfo->Handle = %x\n",cmInfo->Handle);
+    logprintf("cmInfo->Owner = %x\n",cmInfo->Owner);
+    logprintf("cmInfo->Size = %x\n",cmInfo->Size);
+    //共有メモリをマップ、Data要素に確保したバイト列がセットされる。
+    monapi_cmemoryinfo_map(cmInfo);
+    
+    logprintf("monapi_cmemoryinfo_map after!!!\n");
+    
+    //実際に共有メモリへ、何かをセット
+    //addWork = new REPLY_WAIT();
+    //共有メモリ構造体のDataを、目的の型へキャスト
+    addWork = (REPLY_WAIT*)cmInfo->Data;
+    
+    addWork->ip = 10;
+    addWork->wait = 1;
+    sprintf(addWork->mac , "%d",123);
+    
+    logprintf("cmemory use!! after!!!\n");
+    
+    logprintf("ClintSide!! ip=%d wait=%d mac=%s \n", addWork->ip , addWork->wait , addWork->mac);
+    
+
+    //ここで、テスト用サーバに共有メモリのHandleを送る
+    MessageInfo info;
+
+    dword targetID = Message::lookupMainThread("YAMAS.EX2");
+    if (targetID == 0xFFFFFFFF)
+    {
+        printf("local!!!! yamas:INIT not found\n");
+        exit(1);
+    }
+
+    // create message
+    Message::create(&info, MSG_YAMATEST, cmInfo->Handle, cmInfo->Size, 0, NULL);
+    // send
+    if (Message::send(targetID, &info)) {
+        printf("local!!!! yamas:INIT error\n");
+    }
+
+    
     
     return 0;
 
