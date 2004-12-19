c8a9bfff7d895d22e01165b8266c27518d52dedd
bayside <none>
2004-12-19 16:23:06 +0000

    changed new baygui

--- Mona/apps/baygui/gnullpo/gnullpo.cpp
+++ Mona/apps/baygui/gnullpo/gnullpo.cpp
@@ -2,17 +2,6 @@
 Copyright (c) 2004 bayside
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-1. Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-3. The name of the author may not be used to endorse or promote products
-   derived from this software without specific prior written permission.
-
 THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
 IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
 OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -27,26 +16,37 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <baygui.h>
 
-class GNullpo : public Window {
+class GNullpo : public Window
+{
 private:
 	bool pushed;
-	Bitmap *image_normal, *image_pushed;
+	_P<Bitmap> image_normal;
+	_P<Bitmap> image_pushed;
 	
 public:
-	GNullpo(){
-		setRect((800 - 191) / 2, (600 - 205) / 2, 191, 205);
-		setTitle("ぬるぽボタン");
+	GNullpo()
+	{
+		setLocation((800 - 180 - 12) / 2, (600 - 178 - 28) / 2);
+		setClientSize(180,178);
+		setText("ぬるぽボタン");
+	}
+	
+	~GNullpo()
+	{
+		//delete(image_normal);
+		//delete(image_pushed);
+	}
+
+	void onStart()
+	{
+		Window::onStart();
 		pushed = false;
 		image_normal = new Bitmap("/APPS/GNULLPO.APP/NULLPO.BM5");
 		image_pushed = new Bitmap("/APPS/GNULLPO.APP/GAT.BM5");
 	}
-	
-	~GNullpo(){
-		delete(image_normal);
-		delete(image_pushed);
-	}
 
-	void onPaint(Graphics *g) {
+	void onPaint(_P<Graphics> g)
+	{
 		if (pushed == false) {
 			g->drawImage(image_normal, 0, 0);
 		} else {
@@ -54,18 +54,18 @@ public:
 		}
 	}
 	
-	void onEvent(Event *event) {
-		if (event->type == MOUSE_PRESSED) {
+	void onEvent(Event *e)
+	{
+		#if 1
+		if (e->type == WM_MOUSEDOWN) {
 			pushed = true;
-			// 描画高速化
-			//repaint();
-			onPaint(__g);
-		} else if (event->type  == MOUSE_RELEASED) {
+			update();
+		} else if (e->type  == WM_MOUSEUP) {
 			pushed = false;
-			// 描画高速化
-			//repaint();
-			onPaint(__g);
+			update();
 		}
+		#endif
+		Window::onEvent(e);
 	}
 };
 
