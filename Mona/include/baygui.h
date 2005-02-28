c2734eb2f585b29de7d4e9b877e5439d3470ff0a
bayside <none>
2005-02-28 11:43:48 +0000

    added BAYGUI_COPYING

--- Mona/include/baygui.h
+++ Mona/include/baygui.h
@@ -69,8 +69,8 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define inGetUInt32(b) (unsigned int)( (unsigned int)((b)[3])<<24 | (unsigned int)((b)[2])<<16 | (unsigned int)((b)[1])<<8 | (unsigned int)((b)[0]) )
 #define inGetUInt16(b) (unsigned short)( (unsigned short)((b)[1])<<8 | (unsigned short)((b)[0]) )
 
-/** BayGUIバージョン */
-#define BAYGUI_VERSION "BayGUI build $Date$"
+#define BAYGUI_VERSION "BayGUI $Date$"
+#define BAYGUI_COPYING "Copyright (C) 2004-2005, bayside."
 //#define WINDOWMANAGER_MENU_TITLE_JP "ファイル　編集　表示　特別　ヘルプ"
 //#define WINDOWMANAGER_MENU_TITLE_EN "File Edit View Special Help"
 //#define WINDOWMANAGER_STARUP_MESSAGE "starting baygui ...\n"
@@ -86,7 +86,6 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "baygui/Color.h"
 #include "baygui/Object.h"
 #include "baygui/String.h"
-#include "baygui/LinkedItem.h"
 #include "baygui/LinkedList.h"
 #include "baygui/Event.h"
 #include "baygui/KeyEvent.h"
