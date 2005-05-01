3d16befc7ffeb1b53f6c93dad870dbf16739e29a
Higepon (MINOWA Taro) <higepon@gmail.com>
2005-05-01 09:25:32 +0000

    *** empty log message ***

--- Mona/src/servers/file/main.cpp
+++ Mona/src/servers/file/main.cpp
@@ -196,8 +196,6 @@ void MessageLoop()
 
 int MonaMain(List<char*>* pekoe)
 {
-    Log("\n");
-
     if (Message::send(Message::lookupMainThread("INIT"), MSG_SERVER_START_OK) != 0)
     {
         printf("%s: INIT error\n", SVR);
