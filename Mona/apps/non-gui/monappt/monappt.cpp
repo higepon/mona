bb39571e2d3ce97edd577770641964d0dafe7e5d
bayside <none>
2005-08-19 04:57:37 +0000

    include jped draw logic

--- Mona/apps/non-gui/monappt/monappt.cpp
+++ Mona/apps/non-gui/monappt/monappt.cpp
@@ -1,19 +1,83 @@
 #include <monapi.h>
 #include <monapi/CString.h>
 #include <monapi/messages.h>
+#include <jpegls.h>
 
 using namespace MonAPI;
+
+int OpenJpeg(const char* filename)
+{
+	/* check bpp */
+	Screen screen;
+	if (screen.getBpp() < 16) {
+		printf("screen information (x, y) = (%d, %d) %dbpp\n", screen.getWidth(), screen.getHeight(), screen.getBpp());
+		printf("sorry, this demo needs 16bpp or higher Video mode\n");
+		return -1;
+	}
+	
+	CJPEGLS jpeg;
+	
+	monapi_cmemoryinfo* mi = NULL;
+	mi = monapi_call_file_read_data(filename, MONAPI_FALSE);
+
+	if (mi == NULL) {
+		printf("file %s not found", filename);
+		return -1;
+	}
+
+	/* jpeg operation */
+	if (jpeg.Open(mi->Data, mi->Size) != 0) {
+		printf("not supported image\n");
+		monapi_cmemoryinfo_dispose(mi);
+		monapi_cmemoryinfo_delete(mi);
+		return -1;
+	}
+
+	int w, h;
+	jpeg.GetInfo(&w, &h);
+	byte *picture = new byte[w * h * 3];
+	jpeg.Decode(picture);
+
+	int x, y;
+	int vesaWidth  = screen.getWidth();
+	int vesaHeight = screen.getHeight();
+	int vesaBpp	= screen.getBpp() / 8;
+	int ww		 = w < vesaWidth ? w : vesaWidth;
+	int hh		 = h < vesaHeight ? h : vesaHeight;
+	byte* vesaVram = screen.getVRAM();
+
+	for(y = 0; y < hh; y++) {
+		for(x = 0; x < ww; x++) {
+			int k  = (x + (y * vesaWidth)) * vesaBpp;
+			int k2 = (x + (y * w)) * 3;
+			if (vesaBpp == 2) {
+				*(word*)&vesaVram[k] = Color::bpp24to565(picture[k2], picture[k2 + 1], picture[k2 + 2]);
+			} else {
+				vesaVram[k]   = picture[k2 + 2];
+				vesaVram[k+1] = picture[k2 + 1];
+				vesaVram[k+2] = picture[k2 + 0];
+			}
+		}
+	}
+
+	delete [] picture;
+	monapi_cmemoryinfo_dispose(mi);
+	monapi_cmemoryinfo_delete(mi);
+
+	return 0;
+}
 
 int OpenSlide(List<CString>* list, int i)
 {
 	if (list->get(i).endsWith(".jpg")) {
 		// 画像を開く
-		CString cstr = "/APPS/JPEGDEMO.EX2 ";
-		cstr += System::getBundlePath();
+		//CString cstr = "/APPS/JPEGDEMO.EX2 ";
+		CString cstr = System::getBundlePath();
 		cstr += "/";
 		cstr += list->get(i);
-		syscall_set_cursor(0,0);
-		return monapi_call_process_execute_file((const char*)cstr, MONAPI_FALSE);
+		//syscall_set_cursor(0,0);
+		//return monapi_call_process_execute_file((const char*)cstr, MONAPI_FALSE);
+		return OpenJpeg((const char*)cstr);
 	} else if (list->get(i).endsWith(".mpg")) {
 		// 動画を開く
 		CString cstr = "/APPS/MONAPEG.EX2 ";
