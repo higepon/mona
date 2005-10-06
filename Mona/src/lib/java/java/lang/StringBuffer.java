18c13bc735baa6ccf711d031e8b651fee6021b44
bayside <none>
2005-10-06 09:32:52 +0000

    fixed auto extender bug

---
+++ Mona/src/lib/java/java/lang/StringBuffer.java
@@ -0,0 +1,88 @@
+package gnu.gcj.runtime;
+
+public final class StringBuffer {
+	private static final int BUFFER_SIZE = 16;
+	private char[] data = new char[BUFFER_SIZE];
+	private int count = 0;
+	
+	public StringBuffer() {
+	}
+	
+	public StringBuffer(String str) {
+		append(str);
+	}
+	
+	public StringBuffer append(boolean b) {
+		if (b) {
+			append("true");
+		} else {
+			append("false");
+		}
+		return this;
+	}
+	
+	public StringBuffer append(char c) {
+		if (count == data.length) {
+			char[] new_data = new char[data.length * 2];
+			for (int i = 0; i < count; i++) {
+				new_data[i] = data[i];
+			}
+			data = new_data;
+		}
+		data[count++] = c;
+		return this;
+	}
+	
+	public StringBuffer append(int i) {
+		if (i < 0) {
+			append('-');
+			i *= -1;
+		}
+		char[] digit= new char[20];
+		int count = 0;
+		while(i > 0) {
+			digit[count++] = (char)(i % 10 + '0');
+			i /= 10;
+		}
+		for (i = count - 1; i >= 0; i--) {
+			append(digit[i]);
+		}
+		return this;
+	}
+	
+	public StringBuffer append(long l) {
+		append((int)l);
+		return this;
+	}
+	
+	public StringBuffer append(float f) {
+		append((int)f);
+		return this;
+	}
+	
+	public StringBuffer append(double d) {
+		append((int)d);
+		return this;
+	}
+	
+	public StringBuffer append(Object obj) {
+		append(obj.toString());
+		return this;
+	}
+	
+	public StringBuffer append(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			append(str.charAt(i));
+		}
+		return this;
+	}
+	
+	public StringBuffer append(StringBuffer sb) {
+		append(sb.toString());
+		return this;
+	}
+	
+	public String toString() {
+		return new String(data, 0, count);
+	}
+}
