224e6b811cf789783788c89634deadc1a58a4ffd
bayside <none>
2005-10-12 13:01:39 +0000

    added jbaygui classes

---
+++ Mona/src/lib/java/java/awt/Color.java
@@ -0,0 +1,65 @@
+/*
+Copyright (c) 2005 bayside
+
+Permission is hereby granted, free of charge, to any person 
+obtaining a copy of this software and associated documentation files 
+(the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, 
+publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be 
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+package java.awt;
+
+/**
+ 色クラス
+ */
+public class Color {
+	//
+	// システム標準16色に準拠
+	//
+	/** 黒色 */
+	public static final int black     = 0xff000000;
+	/** 濃い茶色 */
+	public static final int maroon    = 0xff800000;
+	/** 濃い緑色 */
+	public static final int green     = 0xff008000;
+	/** 濃い黄色 */
+	public static final int olive     = 0xff808000;
+	/** 濃い青色 */
+	public static final int navy      = 0xff000080;
+	/** 紫色 */
+	public static final int purple    = 0xff800080;
+	/** 暗い青色 */
+	public static final int teal      = 0xff008080;
+	/** 明るい灰色 */
+	public static final int lightGray = 0xffC0C0C0;
+	/** 灰色 */
+	public static final int gray      = 0xff808080;
+	/** 赤色 */
+	public static final int red       = 0xffff0000;
+	/** 明るい緑色 */
+	public static final int lime      = 0xff00ff00;
+	/** 黄色 */
+	public static final int yellow    = 0xffffff00;
+	/** 青色 */
+	public static final int blue      = 0xff0000ff;
+	/** 明るい赤色 */
+	public static final int magenta   = 0xffff00ff;
+	/** 水色 */
+	public static final int cyan      = 0xff00ffff;
+	/** 白色 */
+	public static final int white     = 0xffffffff;
+}
