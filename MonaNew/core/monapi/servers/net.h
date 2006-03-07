0731192498f0a3468bb50e635b02826822ea29b0
Higepon (MINOWA Taro) <higepon@gmail.com>
2006-03-07 05:00:06 +0000

    *** empty log message ***

---
+++ MonaNew/core/monapi/servers/net.h
@@ -0,0 +1,11 @@
+#ifndef _SERVER_NET_
+#define _SERVER_NET_
+
+enum
+{
+    MSG_NET_TCP_CONNECT = 0x69262736, // NET:CON
+    MSG_NET_TCP_SEND    = 0x6927268e, // NET:SND
+    MSG_NET_TCP_RECEIVE = 0x69271156, // NET:RCV
+};
+
+#endif
