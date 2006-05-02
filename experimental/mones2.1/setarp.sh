0e89e3eb51555ed6d0d76a56449223ccf5d5c2a4
eds1275 <none>
2006-05-02 22:46:15 +0000

    support an well-behaved subnetmask setting.

--- experimental/mones2.1/setarp.sh
+++ experimental/mones2.1/setarp.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
-gawk '/generatedAddress\ =/{
-  print $3
-}' ${MONADIR}/tool/mkimg/mona.vmx |\
-sed 's/"//g;s/:/-/g' | xargs arp -s 192.168.10.10 
+
+IP=$( ipconfig | gawk 'BEGIN{FS="[. ]"} /Ethernet/{ if(match($0,/VMnet8/)){ FLAG=1 }else{ FLAG=0 } }
+/Address/{ if( FLAG==1 ){ print $(NF-3)"."$(NF-2)"."$(NF-1)"."$NF+1} }')
+
+gawk '/generatedAddress\ =/{ print $3 }' ${MONADIR}/tool/mkimg/mona.vmx |\
+sed 's/"//g;s/:/-/g' | xargs arp -s $IP
+
 arp -a
