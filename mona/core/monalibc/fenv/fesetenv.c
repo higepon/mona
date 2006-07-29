9b43c83c0b764e1af27bbe78c596adbd02b43ed2
yume_kondou <none>
2006-07-29 11:48:40 +0000

    Fixed bug.

--- mona/core/monalibc/fenv/fesetenv.c
+++ mona/core/monalibc/fenv/fesetenv.c
@@ -2,22 +2,26 @@
 
 int fesetenv(const fenv_t *f)
 {
-	if( f == FE_PC64_ENV )
+	fenv_t *tmp;
+
+	tmp = (fenv_t*)f;
+
+	if( tmp == FE_PC64_ENV )
 	{
 		asm volatile("finit");
 	}
-	else if( f == FE_PC53_ENV )
+	else if( tmp == FE_PC53_ENV )
 	{
-		f->__status_word = 0x2f;
-		asm volatile("fldenv %0" : : "m"(*f));
+		tmp->__status_word = 0x2f;
+		asm volatile("fldenv %0" : : "m"(*tmp));
 	}
-	else if( f == FE_DFL_ENV )
+	else if( tmp == FE_DFL_ENV )
 	{
 		asm volatile("finit");
 	}
 	else
 	{
-		asm volatile("fldenv %0" : : "m"(*f));
+		asm volatile("fldenv %0" : : "m"(*tmp));
 	}
 	return 0;
 }
