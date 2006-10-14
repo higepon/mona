56153ec90a56765de0a88089fe0df36934afb9a9
bayside <none>
2006-10-14 02:20:27 +0000

    added jhello

---
+++ contrib/Misc/jhello/Main.java
@@ -0,0 +1,53 @@
+import java.lang.*;
+import java.io.*;
+
+/*
+import java.util.Date;
+
+public class Main {
+	public static void main(String[] args) {
+		String[] weekdayList = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+		String[] monthList = {
+			"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+			"Jly", "Aug", "Sep", "Oct", "Nov", "Dec"};
+		Date date = new Date();
+		System.out.println(
+			weekdayList[date.getDay()] + " " +
+			monthList[date.getMonth()] + " " +
+			date.getDate() + " " +
+			date.getHours() + ":" +
+			date.getMinutes() + ":" +
+			date.getSeconds() + " " +
+			(date.getYear() + 1900));
+	}
+}
+*/
+/*
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(new Object());
+		System.out.println("abc");
+		System.out.println("abc.length=" + "abc".length());
+	}
+}
+*/
+/*
+public class Main {
+	public static void main(String[] args) {
+		char[] chars = new char[] { 'こ', 'ん', 'に', 'ち', 'は', '世', '界' };
+		String s = "こんにちは世界";
+		for (int i = 0; i < chars.length; i++) {
+			char ch1 = chars[i], ch2 = s.charAt(i);
+			System.out.print((int)ch1);
+			System.out.print(" - ");
+			System.out.print((int)ch2);
+			System.out.println(ch1 == ch2 ? " : OK" : " : NG");
+		}
+	}
+}
+*/
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello,World!");
+	}
+}
