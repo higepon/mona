467918910882368d75b1eb690d192c68d4d24273
yume_kondou <none>
2007-07-14 04:03:53 +0000

    It kills all threads other than a thread who called.

--- mona/core/kernel/shutdown.cpp
+++ mona/core/kernel/shutdown.cpp
@@ -4,22 +4,30 @@
 #include "io.h"
 #include "shutdown.h"
 
-#if 0
+#if 1
 void kill_all_proc()
 {
 	uint32_t *id_list;
 	uint32_t id_num;
 	uint32_t i;
+	uint32_t myself;
+	Thread *t;
 
 	id_list = g_scheduler->GetAllThreadID(&id_num);
+	myself = g_currentThread->thread->id;
 
+	logprintf("\n");
+	logprintf("CurrentThread: %x\n", myself);
 	for( i = 0 ; i < id_num ; i++ )
 	{
-		if( id_list[i] != g_currentThread->thread->id )
-		{
-			ThreadOperation::kill(id_list[i]);
-		}
+		if( id_list[i] == myself ) continue;
+		logprintf("%x\n", id_list[i]);
+		t = g_scheduler->Find(id_list[i]);
+		if( t == NULL ) continue;
+		g_scheduler->Kill(t);
 	}
+	id_list = g_scheduler->GetAllThreadID(&id_num);
+	logprintf("n thread : %d\n", id_num);
 }
 #endif
 
@@ -47,16 +55,18 @@ uint32_t shutdown_by_apm(uint32_t op, uint32_t device)
 {
 	apm_bios_regs regs;
 	uint32_t pstate;
+	bool kill_threads = false;
 
 	switch(op)
 	{
 		case SHUTDOWN_STANDBY: pstate = 1; break;
 		case SHUTDOWN_SUSPEND: pstate = 2; break;
-		case SHUTDOWN_HALT: pstate = 3; break;
+		case SHUTDOWN_HALT: pstate = 3; kill_threads = true; break;
 		default: pstate = 0; break;
 	}
 
-//	kill_all_proc();
+	if( kill_threads ) kill_all_proc();
+
 	logprintf("shutdown by apm: Device = %d, OP = %d\n",device, op);
 
 	regs.eax = 0x5307;
@@ -68,6 +78,7 @@ uint32_t shutdown_by_apm(uint32_t op, uint32_t device)
 
 uint32_t shutdown_by_reboot()
 {
+	kill_all_proc();
 	asm volatile("cli");
 
 	for (;;) {
