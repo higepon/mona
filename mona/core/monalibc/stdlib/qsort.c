df41f8270aaa873f85109a12407199832ab3e464
Higepon (MINOWA Taro) <higepon@gmail.com>
2007-09-09 14:46:31 +0000

    Fixed qsort

--- mona/core/monalibc/stdlib/qsort.c
+++ mona/core/monalibc/stdlib/qsort.c
@@ -1,6 +1,7 @@
 #include <monalibc/stdlib.h>
 #include <monalibc.h>
 
+#if 1
 void qsort(void *base, size_t n, size_t size, int (*fnc)(const void*, const void*)){
 
 	char *stdv = (char *)base; /* pointer to comparison standard value */
@@ -17,7 +18,7 @@ void qsort(void *base, size_t n, size_t size, int (*fnc)(const void*, const void
 
 	right = stdv;
 	for(i = 1; i < n; i++){
-		if(fnc(tmp, stdv) > 0){
+		if(fnc(tmp, stdv) < 0){
 			exchange++;
 			right += size;
 			if(tmp != right) memswap(tmp, right, size);
@@ -31,3 +32,43 @@ void qsort(void *base, size_t n, size_t size, int (*fnc)(const void*, const void
 
 	return;
 }
+#else
+
+// from uClib ssort
+// http://www.uclibc.org/cgi-bin/viewcvs.cgi/trunk/uClibc/libc/stdlib/stdlib.c?rev=14605&view=markup
+void qsort (void  *base,
+            size_t nel,
+            size_t width,
+            int (*comp)(const void *, const void *))
+{
+      size_t wnel, gap, wgap, i, j, k;
+      char *a, *b, tmp;
+
+      wnel = width * nel;
+      for (gap = 0; ++gap < nel;)
+            gap *= 3;
+      while ( gap /= 3 )
+      {
+            wgap = width * gap;
+            for (i = wgap; i < wnel; i += width)
+            {
+                  for (j = i - wgap; ;j -= wgap)
+                  {
+                        a = j + (char *)base;
+                        b = a + wgap;
+                        if ( (*comp)(a, b) <= 0 )
+                              break;
+                        k = width;
+                        do
+                        {
+                              tmp = *a;
+                              *a++ = *b;
+                              *b++ = tmp;
+                        } while ( --k );
+                        if (j < wgap)
+                              break;
+                  }
+            }
+      }
+}
+#endif
