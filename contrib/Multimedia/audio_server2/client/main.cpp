1adf3095b71eae1e278ced3c24bf0bbc85b211ca
yume_kondou <none>
2007-09-12 11:14:34 +0000

    An old audio_server was moved to directory `not_used`.

--- contrib/Multimedia/audio_server2/client/main.cpp
+++ contrib/Multimedia/audio_server2/client/main.cpp
@@ -5,6 +5,12 @@
 #include <assert.h>
 #include <math.h>
 
+int frender(FILE *fp, void *buffer ,size_t size)
+{
+    fread(buffer, size, 1, fp);
+    return feof(fp);
+}
+
 int render(void* buffer, size_t size)
 {
 	short *p = (short*)buffer;
@@ -28,20 +34,25 @@ int main()
 	Audio *audio;
 	char buf[4096];
 	int counter = 0;
+	FILE *fp;
 	struct audio_data_format format = {0, 2, 16, 44100}; 
 
 	audio = new Audio(&format, AUDIO_OUTPUT);
 	printf("Audio object was created.\n");
+	fp = fopen("/APPS/TEST.RAW", "r");
 
 	audio->start();
 	for( ; counter < 44100*10 ; counter += sizeof(buf) )
+//	while(1)
 	{
+		//frender(fp, buf, sizeof(buf));
 		render(buf, sizeof(buf));
 		audio->write(buf, sizeof(buf));
 	}
 	audio->stop();
 
 	delete audio;
+	fclose(fp);
 	printf("AUdio object was deleted.\n");
 	return 0;
 }
