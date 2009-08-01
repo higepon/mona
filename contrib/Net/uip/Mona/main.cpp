7f3a13537abaed0e8988dc0ad127a2745d48b837
Higepon (MINOWA Taro) <higepon@gmail.com>
2009-08-01 14:26:25 +0000

    Net/uip/uip-0.9/Mona/main.cpp : Removed unused files.

--- contrib/Net/uip/Mona/main.cpp
+++ contrib/Net/uip/Mona/main.cpp
@@ -7,135 +7,83 @@ extern "C" {
 // you need a patch for qemu, ask higepon.
 
 //#include "ServerMessageLoop.h"
-#include "monadev.h"
-#include "NicServer.h"
+//#include "monadev.h"
+
+
 #include <monapi.h>
 
-using namespace mones;
-static NicServer* server;
-void NicListenLoop();
-uint32_t nic_read(uint32_t nicThread, Ether::Frame* frame);
-uint32_t nic_write(uint32_t nicThread, OutPacket* packet);
-
-uint32_t nicThread;
-
-#define BUF ((struct uip_eth_hdr *)&uip_buf[0])
-
-extern void MessageLoop();
-extern void* test();
-extern void irc();
-void thread_client()
+class Ether
 {
-//    MessageLoop();
-//    test();
-//    irc();
-    return;
-}
+public:
+#pragma pack(2)
+    typedef struct{
+        uint8_t  dstmac[6];   // 送信先 MAC ID
+        uint8_t  srcmac[6];   // 送信元 MAC ID
+        uint16_t    type;     // フレームタイプ Frame type(DIX) or frame length(IEEE)
+        uint8_t   data[0x600];// Data
+    } Frame;
+#pragma pack(0)
+    enum
+    {
+        ARP = 0x806,
+        IP  = 0x800,
+    };
+};
 
-void thread_init()
+
+class Arp
 {
-    uint32_t id = 3;//(uint32_t)syscall_mthread_create((uint32_t)thread_client);
-// comment out by higepon
-//    syscall_mthread_join(id);
-}
+public:
+    typedef struct
+    {
+        uint16_t hardType  __attribute__((packed));
+        uint16_t protType  __attribute__((packed));
+        uint8_t  hardAddrLen;
+        uint8_t  protAddrLen ;
+        uint16_t opeCode  __attribute__((packed));
+        uint8_t  srcMac[6];
+        uint32_t   srcIp  __attribute__((packed));
+        uint8_t  dstMac[6];
+        uint32_t   dstIp  __attribute__((packed));
+    } Header;
+
+    enum
+    {
+        HARD_TYPE_ETHER= 1,
+        PROTCOL_TYPE_IP=0x0800,
+        /* Operation code. */
+        OPE_CODE_ARP_REQ= 1,
+        OPE_CODE_ARP_REP= 2,
+        OPE_CODE_RARP_REQ=3,
+        OPE_CODE_RARP_REP=4,
+    };
+};
+
+class IP {
+public:
+    enum {
+        ICMP = 0x01,
+        UDP = 0x11,
+    };
+    typedef struct{
+        uint8_t  verhead;  /* バージョン、ヘッダ長。 */
+        uint8_t  tos;      /* TOS. */
+        uint16_t len;       /* トータル長。 */
+        uint16_t id;        /* 識別番号。 */
+        uint16_t frag;      /* フラグ、フラグメントオフセット。 */
+        uint8_t  ttl;      /* Time to Live. */
+        uint8_t  prot;     /* プロトコル番号。 */
+        uint16_t chksum;    /* ヘッダチェックサム。 */
+        uint32_t srcip;        /* 送り元IP。 */
+        uint32_t dstip;        /* 宛先IP。 */
+        char     data[0];
+    } Header;
+};
 
 #ifndef NULL
 #define NULL (void *)0
-#endif /* NULL */
-// int main2(int argc, char* argv[])
-// {
-//     uint32_t id = 3;//syscall_mthread_create((uint32_t)NicListenLoop);
-//     if (MONAPI_FALSE == monapi_notify_server_start("MONITOR.BIN"))
-//     {
-//         exit(-1);
-//     }
+#endif
 
-// // comment out by higepon
-// //    syscall_mthread_join(id);
-
-//     for (;;) {
-//         if (server != NULL && server->isStarted()) {
-//             break;
-//         }
-//         sleep(500);
-//     }
-//     nicThread = server->getThreadID();
-//     u8_t i, arptimer;
-
-//     /* Initialize the device driver. */
-//     monadev_init();
-//     thread_init();
-//     /* Initialize the uIP TCP/IP stack. */
-//     uip_init();
-//     /* Initialize the HTTP server. */
-//     httpd_init();
-
-
-//     arptimer = 0;
-
-//     while(1) {
-//         /* Let the monadev network device driver read an entire IP packet
-//            into the uip_buf. If it must wait for more than 0.5 seconds, it
-//            will return with the return value 0. If so, we know that it is
-//            time to call upon the uip_periodic(). Otherwise, the monadev has
-//            received an IP packet that is to be processed by uIP. */
-//         uip_len = monadev_read(receiver);
-//         if(uip_len == 0) {
-//             for(i = 0; i < UIP_CONNS; i++) {
-//                 uip_periodic(i);
-//                 /* If the above function invocation resulted in data that
-//                    should be sent out on the network, the global variable
-//                    uip_len is set to a value > 0. */
-//                 if(uip_len > 0) {
-//                     uip_arp_out();
-//                     monadev_send();
-//                 }
-//             }
-
-// #if UIP_UDP
-//             for(i = 0; i < UIP_UDP_CONNS; i++) {
-//                 uip_udp_periodic(i);
-//                 /* If the above function invocation resulted in data that
-//                    should be sent out on the network, the global variable
-//                    uip_len is set to a value > 0. */
-//                 if(uip_len > 0) {
-//                     uip_arp_out();
-//                     monadev_send();
-//                 }
-//             }
-// #endif /* UIP_UDP */
-
-//             /* Call the ARP timer function every 10 seconds. */
-//             if(++arptimer == 20) {
-//                 uip_arp_timer();
-//                 arptimer = 0;
-//             }
-
-//         } else {
-//             if(BUF->type == htons(UIP_ETHTYPE_IP)) {
-//                 uip_arp_ipin();
-//                 uip_input();
-//                 /* If the above function invocation resulted in data that
-//                    should be sent out on the network, the global variable
-//                    uip_len is set to a value > 0. */
-//                 if(uip_len > 0) {
-//                     uip_arp_out();
-//                     monadev_send();
-//                 }
-//             } else if(BUF->type == htons(UIP_ETHTYPE_ARP)) {
-//                 uip_arp_arpin();
-//                 /* If the above function invocation resulted in data that
-//                    should be sent out on the network, the global variable
-//                    uip_len is set to a value > 0. */
-//                 if(uip_len > 0) {
-//                     monadev_send();
-//                 }
-//             }
-//         }
-
-//     }
-//     return 0;
-// }
 /*-----------------------------------------------------------------------------------*/
 void
 uip_log(char *m)
@@ -144,46 +92,6 @@ uip_log(char *m)
 }
 /*-----------------------------------------------------------------------------------*/
 
-void NicListenLoop()
-{
-    server = new NicServer;
-    if (!server->initialize())
-    {
-        _printf("NicServer initialize failed\n");
-        exit(1);
-    }
-    server->messageLoop();
-}
-
-uint32_t nic_read(uint32_t nicThread, Ether::Frame* frame)
-{
-    MessageInfo msg;
-    if (MonAPI::Message::sendReceive(&msg, nicThread, MSG_FRAME_READ))
-    {
-        _printf("send error 1");
-        return 1;
-    }
-    if (1 == msg.arg2) {
-        return 1;
-    }
-
-    GetFrameFromSharedMemory(frame);
-    return 0;
-}
-
-// caller should free() packet, after packet written
-// not thread safe
-uint32_t nic_write(uint32_t nicThread, OutPacket* packet)
-{
-    MessageInfo msg;
-    if (MonAPI::Message::sendReceive(&msg, nicThread, MSG_FRAME_WRITE, (uint32_t)packet))
-    {
-        _printf("send error 1");
-        return 1;
-    }
-    return 0;
-}
-#include <monapi.h>
 #include <monapi/CString.h>
 #include <monalibc.h>
 #include <pci/Pci.h>
@@ -953,15 +861,6 @@ monadev_read(VirtioNet* receiver)
     if (!receiver->receive(&frame, &len)) {
         return 0;
     } else {
-        if (frame.type == Util::swapShort(Ether::IP)) {
-            IP::Header* ip = (IP::Header*)frame.data;
-            if (ip->prot == IP::ICMP) {
-                Icmp::Header* icmp = (Icmp::Header*)ip->data;
-                logprintf("RECV ICMP:%x\n", *((uint16_t*)icmp->data + 1));
-            }
-        }
-
-
         memcpy(uip_buf, &frame, len);
         return len;
     }
@@ -970,7 +869,6 @@ monadev_read(VirtioNet* receiver)
 void
 monadev_send(VirtioNet* receiver)
 {
-    int ret;
     char tmpbuf[UIP_BUFSIZE];
     int i;
     for(i = 0; i < 40 + UIP_LLH_LEN; i++) {
@@ -984,20 +882,7 @@ monadev_send(VirtioNet* receiver)
 
     Ether::Frame frame;
     memcpy(&frame, tmpbuf, uip_len);
-
-    if (frame.type == Util::swapShort(Ether::IP)) {
-        IP::Header* ip = (IP::Header*)frame.data;
-        if (ip->prot == IP::ICMP) {
-            Icmp::Header* icmp = (Icmp::Header*)ip->data;
-           logprintf("SEND ICMP:%x\n", *((uint16_t*)icmp->data + 1));
-        }
-
-    }
     receiver->send(&frame, uip_len);
-//     memset(&frame, 0, sizeof(frame));
-//     for (int i = 0; i < 100; i++) {
-//     receiver->send(&frame);
-//     }
     return;
 }
 
@@ -1073,6 +958,7 @@ int main(int argc, char* argv[])
             }
 
         } else {
+#define BUF ((struct uip_eth_hdr *)&uip_buf[0])
             if(BUF->type == htons(UIP_ETHTYPE_IP)) {
     logprintf("(\"%s\" %d)\n", __FILE__, __LINE__);
                 uip_arp_ipin();
