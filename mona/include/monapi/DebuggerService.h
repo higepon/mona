41f3a233da906904a7bff51a353392f1c1fa60ec
Higepon (MINOWA Taro) <higepon@gmail.com>
2009-12-28 17:11:28 +0900

    Added breakpoint function.

---
+++ mona/include/monapi/DebuggerService.h
@@ -0,0 +1,62 @@
+/*
+ * DebuggerService.h - 
+ *
+ *   Copyright (c) 2009  Higepon(Taro Minowa)  <higepon@users.sourceforge.jp>
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ *   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  $Id: DebuggerService.h 261 2008-07-25 06:16:44Z higepon $
+ */
+
+#ifndef _MONAPI_DEBUGGER_SERVICE_
+#define _MONAPI_DEBUGGER_SERVICE_
+
+#include <sys/types.h>
+#include <sys/error.h>
+#include <monapi/syscall.h>
+#include <monapi/Assert.h>
+
+namespace MonAPI {
+
+/*
+    class: DebuggerService
+
+*/
+class DebuggerService
+{
+public:
+
+    /*
+       function: breakpoint
+
+       Issue breakpoint exception. This function also enables GDB remote debug handlers.
+    */
+    static void breakpoint()
+    {
+        asm volatile("int $3");
+    }
+};
+}; // namespace MonAPI
+
+#endif // SCHEME_DEBUGGERSERVICE_
