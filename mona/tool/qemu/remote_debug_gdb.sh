6371d7090fa3b41058dbd0423962d4cfd2a25e57
Higepon (MINOWA Taro) <higepon@gmail.com>
2009-12-30 17:53:49 +0900

    Added remote_debug_gdb.sh for QEMU.

---
+++ mona/tool/qemu/remote_debug_gdb.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+QEMU=qemu
+$QEMU -serial file:/tmp/hige.log -serial tcp::43770,server -cdrom ~/mona/tool/mkimg/mona.iso -fda ~/mona/tool/mkimg/mona.img -boot d  -soundhw es1370 -net user -net nic,model=virtio -redir tcp:8080:10.0.2.15:80
