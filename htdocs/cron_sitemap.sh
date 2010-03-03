c67d1db5a13f6cee9c1daf896d6ef923daf5fdc1
higepon <higepon@users.sourceforge.jp>
2010-03-03 23:32:04 +0900

    Removed unused htdocs.

--- htdocs/cron_sitemap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-/usr/bin/python /home/taro/sitemap_tool/sitemap_gen.py --config=/home/taro/sitemap_tool/mona_config.xml
-/usr/bin/python /home/taro/sitemap_tool/sitemap_gen.py --config=/home/taro/sitemap_tool/higepon_config.xml
-/usr/bin/python /home/taro/sitemap_tool/sitemap_gen.py --config=/home/taro/sitemap_tool/wiki_config.xml
-/usr/bin/python /home/taro/sitemap_tool/sitemap_gen.py --config=/home/taro/sitemap_tool/download_config.xml
