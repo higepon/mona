6242f5ff3b6be540d77515aaeadc430c7ec7b81f
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-03-06 21:10:48 +0900

    Added a test for Message::sendBuffer. (Red).

--- mona/test/monapi/message/message.cpp
+++ mona/test/monapi/message/message.cpp
@@ -5,8 +5,47 @@
 
 using namespace MonAPI;
 
+static const uintptr_t BUFFER_SIZE = 1000;
+static uint8_t buffer[BUFFER_SIZE];
+
+static void __fastcall sendThread(void* mainThread)
+{
+    memset(buffer, BUFFER_SIZE, 0xc1);
+//    intptr_t ret = Message::sendBuffer((uintptr_t)mainThread, buffer, BUFFER_SIZE);
+    intptr_t ret =sendBuffer((uintptr_t)mainThread, buffer, BUFFER_SIZE);
+    EXPECT_EQ(M_OK, ret);
+}
+
 void testSendBuffer()
 {
+    uintptr_t mainThread = System::getThreadID();
+    syscall_mthread_create_with_arg(sendThread, (void*)mainThread);
+    uint8_t* received = NULL;
+    uintptr_t receivedSize = 0;
+    for (;;) {
+        MessageInfo msg;
+        if (Message::receive(&msg) != M_OK) {
+            continue;
+        }
+        if (msg.header == MSG_SEND_BUFFER_START) {
+            uintptr_t bufferSize = msg.arg1;
+            received = new uint8_t[bufferSize];
+            ASSERT_TRUE(received != NULL);
+            uintptr_t sizeToReceive = MESSAGE_INFO_MAX_STR_LENGTH > bufferSize ? bufferSize : MESSAGE_INFO_MAX_STR_LENGTH;
+            memcpy(received + receivedSize, msg.str, sizeToReceive);
+            receivedSize += sizeToReceive;
+        } else if (msg.header == MSG_SEND_BUFFER_PACKET) {
+            uintptr_t restSize = bufferSize - receivedSize;
+            ASSERT_TRUE(restSize != 0);
+            uintptr_t sizeToReceive = MESSAGE_INFO_MAX_STR_LENGTH > restSize ? restSize : MESSAGE_INFO_MAX_STR_LENGTH;
+            memcpy(received + receivedSize, msg.str, sizeToReceive);
+            receivedSize += sizeToReceive;
+        }
+    }
+    EXPECT_EQ(receivedSize, BUFFER_SIZE);
+    EXPECT_EQ(0, memcmp(received, buffer, BUFFER_SIZE));
+    // todo check pid
+
 }
 
 int main(int argc, char *argv[])
