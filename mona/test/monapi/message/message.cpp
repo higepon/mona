f9a16fc5728ccc6cd3601cb1358f56a15e3e0efc
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-03-06 21:18:07 +0900

    A test for Message::sendBuffer is still (Red).

--- mona/test/monapi/message/message.cpp
+++ mona/test/monapi/message/message.cpp
@@ -8,12 +8,23 @@ using namespace MonAPI;
 static const uintptr_t BUFFER_SIZE = 1000;
 static uint8_t buffer[BUFFER_SIZE];
 
+enum {
+    MSG_SEND_BUFFER_START,
+    MSG_SEND_BUFFER_PACKET
+};
+
+static intptr_t sendBuffer(uintptr_t dest, const uint8_t* buffer, uintptr_t bufferSize)
+{
+    return M_OK;
+}
+
 static void __fastcall sendThread(void* mainThread)
 {
     memset(buffer, BUFFER_SIZE, 0xc1);
 //    intptr_t ret = Message::sendBuffer((uintptr_t)mainThread, buffer, BUFFER_SIZE);
     intptr_t ret =sendBuffer((uintptr_t)mainThread, buffer, BUFFER_SIZE);
     EXPECT_EQ(M_OK, ret);
+    exit(0);
 }
 
 void testSendBuffer()
@@ -22,13 +33,14 @@ void testSendBuffer()
     syscall_mthread_create_with_arg(sendThread, (void*)mainThread);
     uint8_t* received = NULL;
     uintptr_t receivedSize = 0;
+    uintptr_t bufferSize = 0;
     for (;;) {
         MessageInfo msg;
         if (Message::receive(&msg) != M_OK) {
             continue;
         }
         if (msg.header == MSG_SEND_BUFFER_START) {
-            uintptr_t bufferSize = msg.arg1;
+            bufferSize = msg.arg1;
             received = new uint8_t[bufferSize];
             ASSERT_TRUE(received != NULL);
             uintptr_t sizeToReceive = MESSAGE_INFO_MAX_STR_LENGTH > bufferSize ? bufferSize : MESSAGE_INFO_MAX_STR_LENGTH;
