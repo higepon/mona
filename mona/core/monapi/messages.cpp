75e02d3dba291e621e32cb575183674857bba6ab
mumurik2 <mumurik2@hotmail.co.jp>
2010-05-10 17:17:30 +0900

    conflict wrongly merged

--- mona/core/monapi/messages.cpp
+++ mona/core/monapi/messages.cpp
@@ -444,13 +444,3 @@ intptr_t monapi_notify_server_start(const char* name)
     return M_OK;
 }
 
-uint32_t monapi_file_delete(const char* file)
-{
-    uint32_t tid = monapi_get_server_thread_id(ID_FILE_SERVER);
-    MessageInfo msg;
-    if (Message::sendReceive(&msg, tid, MSG_FILE_DELETE, 0, 0, 0, file) != 0)
-    {
-        return MONA_FAILURE;
-    }
-    return msg.arg2;
-}
