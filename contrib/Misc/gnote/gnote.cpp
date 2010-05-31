02fba03edcbdd4fce6759af295f87e58363ed808
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-05-31 23:25:26 +0900

    (Green) Enabled stacktrace with map file on GNOTE.

--- contrib/Misc/gnote/gnote.cpp
+++ contrib/Misc/gnote/gnote.cpp
@@ -28,7 +28,13 @@
 */
 #include "controller.hpp"
 
+#define MAP_FILE_PATH "/APPS/BAYGUI/GNOTE.MAP"
+
 int main(int argc, char* argv[]) {
+    uint32_t pid = syscall_get_pid();
+    if (!syscall_stack_trace_enable(pid, MAP_FILE_PATH)) {
+        _printf("syscall_stack_trace_enable failed\n");
+    }
 	gnote::Controller c;
 	if (argc > 1) {
 		c.SetFile(String(argv[1]));
