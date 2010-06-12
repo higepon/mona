54b475375879d564661a10881e33e535de6e548f
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-06-12 23:20:19 +0900

    Changed Assert not to depend on printf which is still broken.

--- mona/include/monapi/Assert.h
+++ mona/include/monapi/Assert.h
@@ -6,7 +6,11 @@
 #ifdef DEBUG_MODE
 #define onAssertError() {exit(1);}
 
-#define ASSERT(condition) {if (!(condition)) {printf("ASSERT failure %s:%d: %s\n", __FILE__, __LINE__, #condition);onAssertError();}}
+#define ASSERT(condition) {\
+    if (!(condition)) {\
+       syscall_print("ASSERT failure see serial log"); \
+       _logprintf("ASSERT failure %s:%d: %s\n", __FILE__, __LINE__, #condition);onAssertError();} \
+    }
 #else
 #define ASSERT(condition) {}
 #endif
