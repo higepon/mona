3551ee1a5179d2f38d792175ed9f33a1552fd383
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-07-13 21:24:52 +0900

    Added test for baygui TextField.

---
+++ contrib/Gui/test_baygui/baygui.cpp
@@ -0,0 +1,93 @@
+/*
+ *   Copyright (c) 2010  Higepon(Taro Minowa)  <higepon@users.sourceforge.jp>
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ *   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <baygui.h>
+#define MUNIT_GLOBAL_VALUE_DEFINED
+#include <monapi/MUnit.h>
+
+using namespace MonAPI;
+
+static void test_TextField()
+{
+    TextField t;
+    _printf("%x", t.getText());
+    EXPECT_STR_EQ("", t.getText());
+
+    t.insertCharacter('A');
+    EXPECT_STR_EQ("A", t.getText());
+
+    t.insertCharacter('B');
+    t.insertCharacter('C');
+    t.insertCharacter('D');
+    EXPECT_STR_EQ("ABCD", t.getText());
+
+    t.deleteCharacter();
+    EXPECT_STR_EQ("ABC", t.getText());
+
+    EXPECT_TRUE(t.cursorLeft());
+    t.insertCharacter('E');
+    EXPECT_STR_EQ("ABEC", t.getText());
+
+    EXPECT_TRUE(t.cursorLeft());
+    t.deleteCharacter();
+    EXPECT_STR_EQ("AEC", t.getText());
+
+    EXPECT_TRUE(t.cursorRight());
+    EXPECT_TRUE(t.cursorRight());
+    t.insertCharacter('Z');
+    EXPECT_STR_EQ("AECZ", t.getText());
+}
+
+static void test_TextField_cursor()
+{
+    const char* text = "Hello";
+    TextField t;
+    t.setText(text);
+
+    for (int i = 0; i < strlen(text); i++) {
+        EXPECT_TRUE(t.cursorLeft());
+    }
+    EXPECT_EQ(false, t.cursorLeft());
+
+    for (int i = 0; i <= strlen(text); i++) {
+        EXPECT_TRUE(t.cursorRight());
+    }
+    EXPECT_EQ(false, t.cursorRight());
+
+    t.insertCharacter('Z');
+    EXPECT_STR_EQ("HelloZ", t.getText());
+}
+
+int main(int argc, char* argv[])
+{
+    test_TextField();
+    test_TextField_cursor();
+
+    TEST_RESULTS(baygui);
+    return 0;
+}
+
