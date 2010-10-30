765e031813bfebeca1fc3ce1d6b62422833c808e
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-10-30 22:06:45 +0900

    Fixed backup image.

--- mona/tool/qemu/start.sh
+++ mona/tool/qemu/start.sh
@@ -3,8 +3,8 @@ QEMU=qemu
 #QEMU=~/Desktop/qemu-0.12.5/i386-softmmu/qemu
 # -k ja option is required by "Caps Lock and Ctrl swap".
 # the last virtio disk is used for tests.
-#
-$QEMU  -vnc :2 -m 512 -enable-kvm -k ja -serial file:$HOME/mona-qemu.log -cdrom ../mkimg/mona.iso -fda ../mkimg/mona.img -boot d  -soundhw es1370 -net user -net nic,model=virtio -redir tcp:8080:10.0.2.15:80 -redir tcp:8007:10.0.2.15:7 -redir tcp:8181:10.0.2.15:8181 \
+# -vnc :2
+$QEMU -m 512 -enable-kvm -k ja -serial file:$HOME/mona-qemu.log -cdrom ../mkimg/mona.iso -fda ../mkimg/mona.img -boot d  -soundhw es1370 -net user -net nic,model=virtio -redir tcp:8080:10.0.2.15:80 -redir tcp:8007:10.0.2.15:7 -redir tcp:8181:10.0.2.15:8181 \
 -drive file=../mkimg/mona.iso,media=cdrom,if=virtio \
 -drive file=../mkimg/mona.img,media=disk,if=virtio \
 -drive file=../mkimg/test.img,media=disk,if=virtio \
