345d1ec6fc746ef449fd252a0de538c4f571307d
Higepon (MINOWA Taro) <higepon@gmail.com>
2011-01-06 08:14:33 +0900

    HTTPS get doesn't work. Why?

--- mona/test/monapi/socket/socket.cpp
+++ mona/test/monapi/socket/socket.cpp
@@ -8,6 +8,9 @@
 #include <monapi.h>
 #include <stdio.h>
 #include <time.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/rand.h>
 
 using namespace MonAPI;
 
@@ -225,6 +228,193 @@ static void testOpenManyTimes()
     }
 }
 
+#define BUF_LEN 256
+static void testSSLHttpGet()
+{
+    // ASSERT_EQ(8, sizeof(unsigned long long));
+    // int ret;
+    // int s;
+    // SSL *ssl;
+    // SSL_CTX *ctx;
+    // char request[BUF_LEN];
+    // char *host = "www.hatena.ne.jp";
+    // char *path = "/";
+
+    // struct addrinfo hints;
+    // struct addrinfo* res;
+
+    // memset(&hints, 0, sizeof(hints));
+    // hints.ai_socktype = SOCK_STREAM;
+    // hints.ai_family = AF_INET;
+
+    // ASSERT_EQ(0, getaddrinfo(host, "443", &hints, &res));
+
+    // int sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    // ASSERT_TRUE(sock != -1);
+
+    // ASSERT_EQ(0, connect(sock, res->ai_addr, res->ai_addrlen));
+
+    // SSL_load_error_strings();
+    // SSL_library_init();
+    // ctx = SSL_CTX_new(SSLv23_client_method());
+    // ASSERT_TRUE(ctx != NULL);
+
+    // ssl = SSL_new(ctx);
+    // ASSERT_TRUE(ssl != NULL);
+
+    // ASSERT_EQ(1, SSL_set_fd(ssl, s));
+
+    // RAND_poll();
+    // while ( RAND_status() == 0 ){
+    //     unsigned short rand_ret = rand() % 65536;
+    //     RAND_seed(&rand_ret, sizeof(rand_ret));
+    // }
+
+    // ret = SSL_connect(ssl);
+    // ASSERT_EQ(1, ret);
+
+    // sprintf(request,
+    //         "GET %s HTTP/1.0\r\n"
+    //         "Host: %s\r\n\r\n",
+    //         path, host);
+
+    // ret = SSL_write(ssl, request, strlen(request));
+    // ASSERT_TRUE(ret > 0);
+    // while (1){
+    //     char buf[BUF_LEN];
+    //     int read_size;
+    //     read_size = SSL_read(ssl, buf, sizeof(buf)-1);
+
+    //     if ( read_size > 0 ){
+    //         buf[read_size] = '\0';
+    //     } else if ( read_size == 0 ){
+    //         break;
+    //     } else {
+    //         ASSERT_TRUE(false);
+    //     }
+    // }
+
+    // EXPECT_EQ(1, SSL_shutdown(ssl));
+    // closesocket(s);
+
+    // SSL_free(ssl);
+    // SSL_CTX_free(ctx);
+    // ERR_free_strings();
+    ASSERT(sizeof(unsigned long long) == 8);
+  int ret;
+  int s;
+
+  SSL *ssl;
+  SSL_CTX *ctx;
+
+  char request[BUF_LEN];
+
+  char *host = "www.hatena.ne.jp";
+  char *path = "/";
+
+
+  struct addrinfo hints;
+  struct addrinfo* res;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_family = AF_INET;
+
+    if (0 != getaddrinfo(host, "443", &hints, &res)) {
+        monapi_fatal("hige");
+    }
+
+    int sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sock == -1) {
+        monapi_fatal("hige");
+    }
+
+    ret = connect(sock, res->ai_addr, res->ai_addrlen);
+    if (ret != 0) {
+        monapi_fatal("hige");
+    }
+
+
+  SSL_load_error_strings();
+  SSL_library_init();
+  ctx = SSL_CTX_new(SSLv23_client_method());
+  if ( ctx == NULL ){
+    ERR_print_errors_fp(stderr);
+    exit(1);
+  }
+
+  ssl = SSL_new(ctx);
+  if ( ssl == NULL ){
+    ERR_print_errors_fp(stderr);
+    exit(1);
+  }
+
+  ret = SSL_set_fd(ssl, s);
+  if ( ret == 0 ){
+    ERR_print_errors_fp(stderr);
+    exit(1);
+  }
+
+  /* PRNG 初期化 */
+  RAND_poll();
+  while ( RAND_status() == 0 ){
+    unsigned short rand_ret = rand() % 65536;
+    RAND_seed(&rand_ret, sizeof(rand_ret));
+  }
+
+  /* SSL で接続 */
+  ret = SSL_connect(ssl);
+  if ( ret != 1 ){
+      ERR_print_errors_fp(stderr);
+      exit(1);
+  }
+
+  /* リクエスト送信 */
+  sprintf(request,
+      "GET %s HTTP/1.0\r\n"
+      "Host: %s\r\n\r\n",
+          path, host);
+
+  ret = SSL_write(ssl, request, strlen(request));
+  if ( ret < 1 ){
+    ERR_print_errors_fp(stderr);
+    exit(1);
+  }
+
+  printf("サーバからのレスポンス\n");
+  while (1){
+    char buf[BUF_LEN];
+    int read_size;
+    read_size = SSL_read(ssl, buf, sizeof(buf)-1);
+
+    if ( read_size > 0 ){
+      buf[read_size] = '\0';
+//      _logprintf("%s", buf);
+      fwrite(buf, read_size, 1, stdout);
+    } else if ( read_size == 0 ){
+      /* FIN 受信 */
+      break;
+    } else {
+      ERR_print_errors_fp(stderr);
+      exit(1);
+    }
+  }
+
+  ret = SSL_shutdown(ssl);
+  if ( ret != 1 ){
+    ERR_print_errors_fp(stderr);
+    exit(1);
+  }
+//  close(s);
+
+  SSL_free(ssl);
+  SSL_CTX_free(ctx);
+  ERR_free_strings();
+
+
+}
+
+
 int main(int argc, char *argv[])
 {
     testSendInvalidFd();
@@ -235,6 +425,7 @@ int main(int argc, char *argv[])
     testSocketOption();
     testEcho();
     testOpenManyTimes();
+    testSSLHttpGet();
 
     // todo close
 
