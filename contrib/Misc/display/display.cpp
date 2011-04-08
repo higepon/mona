3ad59977141869221e201b97696ccdd7ecd8e1a8
Higepon (MINOWA Taro) <higepon@gmail.com>
2011-04-08 23:17:54 +0900

    Now display application run as two threads one is main and the other is updater.

--- contrib/Misc/display/display.cpp
+++ contrib/Misc/display/display.cpp
@@ -9,6 +9,7 @@ static void __fastcall updateFeedAsync(void* arg);
 
 class Display : public Frame {
 private:
+    uintptr_t updaterId_;
     scoped_ptr<TextField> inputArea_;
     scoped_ptr<Button> pushButton_;
     scoped_ptr<Button> updateButton_;
@@ -31,8 +32,10 @@ public:
         forbidden_comma
     };
 
-    Display() : inputArea_(new TextField()),
-                pushButton_(new Button("Post")),
+    Display(uintptr_t updaterId) :
+        updaterId_(updaterId),
+        inputArea_(new TextField()),
+        pushButton_(new Button("Post")),
                 updateButton_(new Button("Update")),
                 updating_(false),
                 idleTimeMsec_(5000)
@@ -47,7 +50,6 @@ public:
         add(inputArea_.get());
         add(pushButton_.get());
         add(updateButton_.get());
-        setTimer(TIMER_INTERVAL);
     }
 
     ~Display()
@@ -62,7 +64,8 @@ public:
         static int i = 0;
         WebImage* image = new WebImage(url, file);
         image->initialize();
-        image->resize(20, 20);
+// aririnn's smallest image.
+//        image->resize(20, 20);
         TextField* field = new TextField();
         fields_.push_back(field);
         images_.push_back(image);
@@ -87,7 +90,10 @@ public:
     void updateFeed()
     {
         setStatusUpdating();
-        syscall_mthread_create_with_arg(updateFeedAsync, this);
+        int ret = Message::send(updaterId_, MSG_UPDATE);
+        if (ret != M_OK) {
+            monapi_fatal("MSG_UPDATE send failed");
+        }
     }
 
     void processEvent(Event* event)
@@ -100,15 +106,35 @@ public:
             if (event->getType() == MouseEvent::MOUSE_RELEASED) {
                 updateFeed();
             }
-        } else if (event->getType() == Event::TIMER) {
-            if (!updating_) {
-                idleTimeMsec_ += TIMER_INTERVAL;
-                if (idleTimeMsec_ > 1000) {
-                    updateFeed();
+        } else if (event->getType() == Event::CUSTOM_EVENT) {
+            if (event->header == MSG_OK) {
+                scoped_ptr<SharedMemory> shm(monapi_file_read_all("/USER/TEMP/fb.data"));
+                if (shm.get() == NULL) {
+                    monapi_fatal("can't read fb.data");
                 }
+                std::string text((char*)shm->data());
+                Strings lines = StringHelper::split("\n", text);
+                for (size_t i = 0; i < lines.size() && i < Display::MAX_ROWS; i++) {
+                    Strings line = StringHelper::split("$", lines[i]);
+                    std::string imageUri = "http://graph.facebook.com/";
+                    std::string filename = "/USER/TEMP/" + line[0] + ".JPG";
+                    imageUri += line[0];
+                    imageUri += "/picture";
+                    createOnePost(imageUri, filename, line[2]);
+                }
+                setStatusDone();
             }
-            setTimer(TIMER_INTERVAL);
         }
+
+//  else if (event->getType() == Event::TIMER) {
+        //     if (!updating_) {
+        //         idleTimeMsec_ += TIMER_INTERVAL;
+        //         if (idleTimeMsec_ > 1000) {
+        //             updateFeed();
+        //         }
+        //     }
+        //     setTimer(TIMER_INTERVAL);
+        // }
     }
 
     void setStatusDone()
@@ -207,6 +233,50 @@ static void __fastcall updateFeedAsync(void* arg)
     exit(0);
 }
 
+class Updater
+{
+public:
+    void run()
+    {
+        for (MessageInfo msg;;)
+        {
+            if (Message::receive(&msg)) continue;
+            switch (msg.header)
+            {
+            case MSG_UPDATE:
+            {
+                update();
+                int ret = Message::send(msg.from, MSG_OK);
+                if (ret != M_OK) {
+                    monapi_fatal("MSG_UPDATE send failed");
+                }
+                break;
+            }
+            default:
+                monapi_warn("unknown message %d\n", msg.header);
+                break;
+            }
+        }
+    }
+private:
+    void update()
+    {
+        std::string command(System::getMoshPath());
+        command += " /LIBS/MOSH/bin/fb-feed-get.sps";
+        uint32_t tid;
+        intptr_t result = monapi_call_process_execute_file_get_tid(command.c_str(), MONAPI_TRUE, &tid, System::getProcessStdinID(), System::getProcessStdoutID());
+        if (result != 0) {
+            monapi_fatal("can't exec Mosh");
+        }
+        monapi_process_wait_terminated(tid);
+    }
+};
+
+static void __fastcall updaterLauncher(void* arg)
+{
+    Updater updater;
+    updater.run();
+}
 
 int main(int argc, char* argv[])
 {
@@ -216,8 +286,9 @@ int main(int argc, char* argv[])
     if (ret != M_OK) {
         _printf("syscall_stack_trace_enable failed%d\n", ret);
     }
-    Display display;
-    logprintf("display=%x", &display);
+
+    uintptr_t updaterId = syscall_mthread_create_with_arg(updaterLauncher, NULL);
+    Display display(updaterId);
     display.run();
     return 0;
 }
