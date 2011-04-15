4852bfd6f22a2fd7ed5b3931ec904d335f94b110
Higepon (MINOWA Taro) <higepon@gmail.com>
2011-04-15 22:20:04 +0900

    (Refactor) Moved all functions to main.

--- mona/core/process_server/ProcessManager.h
+++ mona/core/process_server/ProcessManager.h
@@ -1,12 +1,12 @@
-#ifndef __PROCESS_SERVER_PROCESS_MANAGER_H__
-#define __PROCESS_SERVER_PROCESS_MANAGER_H__
+// #ifndef __PROCESS_SERVER_PROCESS_MANAGER_H__
+// #define __PROCESS_SERVER_PROCESS_MANAGER_H__
 
-#include "ProcessInfo.h"
+// #include "ProcessInfo.h"
 
-extern void initCommonParameters();
-extern ProcessInfo getProcessInfo(uint32_t tid);
-extern void addProcessInfo(uint32_t tid, uint32_t parent, const MonAPI::CString& path);
-extern void addProcessInfo(uint32_t tid, uint32_t parent, const MonAPI::CString& name, const MonAPI::CString& path, uint32_t stdin_id, uint32_t stdout_id);
-extern bool processHandler(MessageInfo* msg);
+// extern void initCommonParameters();
+// extern ProcessInfo getProcessInfo(uint32_t tid);
+// extern void addProcessInfo(uint32_t tid, uint32_t parent, const MonAPI::CString& path);
+// extern void addProcessInfo(uint32_t tid, uint32_t parent, const MonAPI::CString& name, const MonAPI::CString& path, uint32_t stdin_id, uint32_t stdout_id);
+// extern bool processHandler(MessageInfo* msg);
 
-#endif  // __PROCESS_SERVER_PROCESS_MANAGER_H__
+// #endif  // __PROCESS_SERVER_PROCESS_MANAGER_H__
