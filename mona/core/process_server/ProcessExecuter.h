b392278544eb2a25cb51e77bff8a0f457c86113a
Higepon (MINOWA Taro) <higepon@gmail.com>
2011-04-16 13:22:54 +0900

    (Refactor) Place two classes to two files.

---
+++ mona/core/process_server/ProcessExecuter.h
@@ -0,0 +1,165 @@
+/*
+ *
+ *   Copyright (c) 2011  Higepon(Taro Minowa)  <higepon@users.sourceforge.jp>
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ *   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef _PROCESS_EXECUTER_
+#define _PROCESS_EXECUTER_
+
+class ProcessExecuter {
+    int ExecuteProcess(uint32_t parent, MonAPI::SharedMemory& shm, uint32_t entryPoint, const MonAPI::CString& path, const MonAPI::CString& name, CommandOption* option, bool prompt, uint32_t stdin_id, uint32_t stdout_id, uint32_t* tid, uint32_t observer)
+        {
+            LoadProcessInfo info;
+            info.image = shm.data();
+            info.size  = shm.size();
+            info.entrypoint = entryPoint;
+            info.name = name;
+            info.list = option;
+            info.observer = observer;
+
+            intptr_t ret = syscall_load_process_image(&info);
+            if (ret != M_OK) {
+                monapi_warn("failed to execute process %s", (const char*)path);
+            }
+            *tid = info.tid;
+            _logprintf("[execute process name=%s tid=%x stdout_id =%x stdin_id = %x]", (const char*)name, *tid, stdout_id, stdin_id);
+
+            return ret;
+        }
+
+public:
+    MonAPI::CString GetFileName(const MonAPI::CString& path)
+        {
+            int p = path.lastIndexOf('/');
+            if (p < 0) return path;
+
+            p++;
+            return path.substring(p, path.getLength() - p);
+        }
+
+    int ExecuteFile(uint32_t parent, const MonAPI::CString& commandLine, uint32_t stdin_id, uint32_t stdout_id, uint32_t* tid, uint32_t observer)
+        {
+            bool prompt = false; /* todo */
+            /* list initilize */
+            CommandOption list;
+            list.next = NULL;
+            CommandOption* option = NULL;
+            MonAPI::CString path;
+            _A<MonAPI::CString> args = commandLine.split(' ');
+            FOREACH (MonAPI::CString, arg, args)
+            {
+                if (arg == NULL) continue;
+
+                if (path == NULL)
+                {
+                    path = arg.toUpper();
+                    continue;
+                }
+                option = new CommandOption;
+                strncpy(option->str, arg, sizeof(option->str));
+                option->next = list.next;
+                list.next = option;
+            }
+            END_FOREACH
+                uint64_t s2 = MonAPI::Date::nowInMsec();
+            MonAPI::SharedMemory* shm = NULL;
+            uint32_t entryPoint = 0xa0000000;
+            int result = 1;
+            const char* svr_id = NULL;
+            uint64_t s3, s4;
+            if (path.endsWith(".ELF") || path.endsWith(".EL2") || path.endsWith(".EL5"))
+            {
+                svr_id = "/servers/elf";
+            }
+            else if (path.endsWith(".EXE") || path.endsWith(".EX2") || path.endsWith(".EX5"))
+            {
+                svr_id = "/servers/pe";
+            }
+            if (svr_id != NULL)
+            {
+                MessageInfo msg;
+                uint32_t tid;
+                if (monapi_name_whereis(svr_id, tid) != M_OK) {
+                    monapi_fatal("server not found");
+                }
+                if (MonAPI::Message::sendReceive(&msg, tid, MSG_PROCESS_CREATE_IMAGE, MONAPI_TRUE /* TODO */, 0, 0, path) != M_OK) {
+                    _printf("Error %s:%d\n", __FILE__, __LINE__);
+                    exit(-1);
+                }
+                if (msg.arg2 != 0) {
+                    result = 0;
+                    entryPoint = msg.arg3;
+                    shm = new MonAPI::SharedMemory(msg.arg2, atoi(msg.str));
+                    if (shm->map(true) != M_OK) {
+                        monapi_fatal("Error %s:%d\n", __FILE__, __LINE__);
+                        exit(-1);
+                    }
+                    // notify map is done.
+                    int status = MonAPI::Message::reply(&msg);
+                    if (status != M_OK) {
+                        monapi_warn("%s reply failed : %s\n", __func__, monapi_error_string(status));
+                    }
+
+                }
+                else
+                {
+                    result = msg.arg3;
+                }
+            }
+            else if (path.endsWith(".BN2"))
+            {
+                shm = monapi_call_file_decompress_bz2_file(path, prompt ? MONAPI_TRUE : MONAPI_FALSE);
+            }
+            else if (path.endsWith(".BN5"))
+            {
+                shm = monapi_call_file_decompress_st5_file(path, prompt ? MONAPI_TRUE : MONAPI_FALSE);
+            }
+            else
+            {
+                shm = monapi_file_read_all(path);
+            }
+
+            if (shm == NULL)
+            {
+                return result;
+            }
+            else
+            {
+                result = ExecuteProcess(parent, *shm, entryPoint, path, GetFileName(path), &list, prompt, stdin_id, stdout_id, tid, observer);
+                delete shm;
+            }
+            CommandOption* next;
+            for (option = list.next; option; option = next)
+            {
+                next = option->next;
+                delete option;
+            }
+            return result;
+        }
+};
+
+#endif // _PROCESS_EXECUTER_
--- mona/core/process_server/ProcessServer.h
+++ mona/core/process_server/ProcessServer.h
@@ -1,6 +1,146 @@
 #ifndef __PROCESS_SERVER_H__
 #define __PROCESS_SERVER_H__
 
-#define SVR "Process Server"
+#include <vector>
+#include <monapi.h>
+#include "ProcessInfo.h"
+#include "ProcessExecuter.h"
+
+class ProcessServer {
+private:
+    MonAPI::scoped_ptr<MonAPI::SharedMemory> commonParams_;
+    std::vector<ProcessInfo> infos_;
+    ProcessExecuter executer_;
+
+public:
+    ProcessServer() : commonParams_(new MonAPI::SharedMemory(sizeof(CommonParameters)))
+    {
+        if (commonParams_->map(false) != M_OK) {
+            monapi_fatal("initCommonParameters failed");
+        }
+        if (monapi_notify_server_start("INIT") != M_OK) {
+            monapi_fatal("monapi_notify_server_start to INIT failed");
+        }
+
+        if (monapi_name_add("/servers/process") != M_OK) {
+            monapi_fatal("monapi_name_add failed");
+        }
+    }
+
+private:
+
+    void removeProcessInfo(uint32_t tid)
+    {
+        for (std::vector<ProcessInfo>::iterator it = infos_.begin(); it != infos_.end(); ++it) {
+            if ((*it).tid == tid) {
+                infos_.erase(it);
+                return ;
+            }
+        }
+        ASSERT(false);
+    }
+
+    intptr_t addProcessInfo(uint32_t parentTid, uint32_t subThreadTid)
+    {
+        ProcessInfo parent = getProcessInfo(parentTid);
+        if (parent.tid == THREAD_UNKNOWN) {
+            return M_NOT_FOUND;
+        }
+        infos_.push_back(ProcessInfo(subThreadTid, parent));
+        return M_OK;
+    }
+
+    ProcessInfo getProcessInfo(uint32_t tid)
+    {
+        for (std::vector<ProcessInfo>::const_iterator it = infos_.begin(); it != infos_.end(); ++it) {
+            if ((*it).tid == tid) {
+                return *it;
+            }
+        }
+        return ProcessInfo();
+    }
+
+    void addProcessInfo(uint32_t tid, uint32_t parent, const MonAPI::CString& name, const MonAPI::CString& path, uint32_t stdin_id, uint32_t stdout_id)
+    {
+        ProcessInfo pi(tid, parent, name, path, stdin_id, stdout_id);
+        infos_.push_back(pi);
+    }
+
+    MonAPI::CString GetFileNameFromPath(const MonAPI::CString& path)
+    {
+        int p = path.lastIndexOf('/');
+        if (p < 0) return path;
+        p++;
+        return path.substring(p, path.getLength() - p);
+    }
+
+    MonAPI::CString GetPathFromCommandLins(const MonAPI::CString& commandLine)
+    {
+        MonAPI::CString path;
+        _A<MonAPI::CString> args = commandLine.split(' ');
+        FOREACH (MonAPI::CString, arg, args)
+        {
+            if (arg == NULL) {
+                continue;
+            }
+            if (path == NULL) {
+                path = arg.toUpper();
+                break;
+            }
+        }
+        END_FOREACH
+        return NULL;
+    }
+
+    int ExecuteFile(uint32_t parent, const MonAPI::CString& commandLine, uint32_t stdin_id, uint32_t stdout_id, uint32_t* tid, uint32_t observer)
+    {
+        intptr_t ret = executer_.ExecuteFile(parent, commandLine, stdin_id, stdout_id, tid, observer);
+        if (ret == M_OK) {
+            MonAPI::CString path = GetPathFromCommandLins(commandLine);
+            addProcessInfo(*tid, parent, GetFileNameFromPath(path), path, stdin_id, stdout_id);
+        }
+        return ret;
+    }
+
+public:
+
+    void service()
+    {
+        for (MessageInfo msg;;) {
+            if (MonAPI::Message::receive(&msg) != 0) continue;
+
+            switch (msg.header) {
+            case MSG_PROCESS_EXECUTE_FILE:
+            {
+                uint32_t tid = 0;
+                int result = ExecuteFile(msg.from, msg.str, msg.arg2, msg.arg3, &tid, msg.from);
+                MonAPI::Message::reply(&msg, result, tid);
+                break;
+            }
+            case MSG_PROCESS_GET_PROCESS_STDIO:
+            {
+                ProcessInfo pi = getProcessInfo(msg.arg1);
+                MonAPI::Message::reply(&msg, pi.stdin_id, pi.stdout_id);
+                break;
+            }
+            case MSG_PROCESS_TERMINATED:
+                removeProcessInfo(msg.arg1);
+                break;
+            case MSG_PROCESS_GET_COMMON_PARAMS:
+                MonAPI::Message::reply(&msg, commonParams_->handle());
+                break;
+            case MSG_PROCESS_REGISTER_THREAD:
+            {
+                uint32_t parentTid = msg.from;
+                uint32_t subThreadTid = msg.arg1;
+                intptr_t ret = addProcessInfo(parentTid, subThreadTid);
+                MonAPI::Message::reply(&msg, ret);
+                break;
+            }
+            }
+        }
+    }
+};
+
 
 #endif  // __PROCESS_SERVER_H__
