86707b4f405d5de06e483b5a0189f487325ee993
Higepon (MINOWA Taro) <higepon@gmail.com>
2011-04-16 13:38:40 +0900

    (Refactor) Removed unused .cpp files.

--- mona/core/process_server/ProcessInfo.h
+++ mona/core/process_server/ProcessInfo.h
@@ -10,13 +10,51 @@ struct ProcessInfo
     MonAPI::CString name, path;
     uint32_t stdin_id, stdout_id;
 
-    ProcessInfo();
-    ProcessInfo(uint32_t tid, const ProcessInfo& parent);
-    ProcessInfo(const ProcessInfo& pi);
-    ProcessInfo(uint32_t tid, uint32_t parent, const MonAPI::CString& name, const MonAPI::CString& path, uint32_t stdin_id, uint32_t stdout_id);
+    ProcessInfo() :
+        tid(THREAD_UNKNOWN),
+        parent(THREAD_UNKNOWN),
+        name("unknown"),
+        path("unknown"),
+        stdin_id(THREAD_UNKNOWN),
+        stdout_id(THREAD_UNKNOWN)
+    {
+    }
+
+    ProcessInfo(const ProcessInfo& pi)
+        : tid(pi.tid), parent(pi.parent), name(pi.name), path(pi.path), stdin_id(pi.stdin_id), stdout_id(pi.stdout_id)
+    {
+    }
+
+    // Inherits from Parent
+    ProcessInfo(uint32_t tid, const ProcessInfo& parent) :
+        tid(tid),
+        parent(parent.tid),
+        name(parent.name),
+        path(parent.path),
+        stdin_id(parent.stdin_id),
+        stdout_id(parent.stdout_id)
+    {
+    }
+
+
+    ProcessInfo(uint32_t tid, uint32_t parent, const MonAPI::CString& name, const MonAPI::CString& path, uint32_t stdin_id, uint32_t stdout_id)
+        : tid(tid), parent(parent), name(name), path(path), stdin_id(stdin_id), stdout_id(stdout_id)
+    {
+    }
+
+    ProcessInfo& operator =(const ProcessInfo& pi)
+    {
+        this->tid       = pi.tid;
+        this->parent    = pi.parent;
+        this->name      = pi.name;
+        this->path      = pi.path;
+        this->stdin_id  = pi.stdin_id;
+        this->stdout_id = pi.stdout_id;
+        return *this;
+    }
+
     inline bool operator ==(const ProcessInfo& pi) { return this->tid == pi.tid; }
     inline bool operator !=(const ProcessInfo& pi) { return this->tid != pi.tid; }
-    ProcessInfo& operator =(const ProcessInfo& pi);
 };
 
 #endif  // __PROCESS_SERVER_PROCESS_INFO_H__
--- mona/core/process_server/main.cpp
+++ mona/core/process_server/main.cpp
@@ -3,20 +3,7 @@
 #include <monapi/messages.h>
 #include <monapi/Array.h>
 #include <monalibc.h>
-#include "ProcessServer.h"
-#include "ProcessManager.h"
-#include "ProcessServer.h"
-#include "ProcessManager.h"
-
-#include <monapi.h>
-#include <monapi/messages.h>
 #include <monapi/Assert.h>
-#include <vector>
-#include "ProcessInfo.h"
-using namespace std;
-using namespace MonAPI;
-
-
 #include "ProcessServer.h"
 
 int main(int argc, char* argv[])
