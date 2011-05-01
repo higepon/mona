bc3b5e1834c13f179689729561e68067ec92ee6b
Higepon (MINOWA Taro) <higepon@gmail.com>
2011-05-01 11:34:48 +0900

    (Red) Added testAddedCachedCanGet().

--- mona/test/drivers/block_cache/block_cache.cpp
+++ mona/test/drivers/block_cache/block_cache.cpp
@@ -34,11 +34,25 @@ static void testEmptyCacheHasNoCacheOf0thSector()
     EXPECT_EQ(false, bc.get(startSector, numSectors, cacheList));
     EXPECT_EQ(0, cacheList.size());
 }
+// todo match sector
+static void testAddedCachedCanGet()
+{
+    BlockCache bc(MAX_CACHE_SIZE);
+    const int targetSector = 0;
+    CacheList cacheList;
+    const int numSectors = 1;
+    EXPECT_TRUE(bc.add(targetSector, NULL));
+    EXPECT_EQ(true, bc.get(targetSector, numSectors, cacheList));
+    ASSERT_EQ(1, cacheList.size());
+    Cache foundCache = cacheList[0];
+    EXPECT_EQ(0, foundCache.sector());
+    EXPECT_EQ(NULL, foundCache.get());
+}
 
 int main(int argc, char *argv[])
 {
     testEmptyCacheHasNoCacheOf0thSector();
+    testAddedCachedCanGet();
     TEST_RESULTS();
     return 0;
 }
-
