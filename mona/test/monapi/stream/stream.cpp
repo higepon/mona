a6ed160058495aabc022bf541066259e1007a16c
Higepon (MINOWA Taro) <higepon@gmail.com>
2011-06-01 18:47:13 +0900

    (Green) Added and passed testOneProcessReadsEmptyStreamImmediatlyReturnsZero().

--- mona/test/monapi/stream/stream.cpp
+++ mona/test/monapi/stream/stream.cpp
@@ -5,14 +5,22 @@
 
 using namespace MonAPI;
 
+static void testOneProcessReadsEmptyStreamImmediatlyReturnsZero()
+{
+    Stream s;
+    uint8_t buf[3];
+    EXPECT_EQ(0, s.read(buf, sizeof(buf)));
+}
+
 int main(int argc, char *argv[])
 {
-    // testOneProcessReadsEmptyStreamImmediatlyReturnsZero();
+    testOneProcessReadsEmptyStreamImmediatlyReturnsZero();
     // testOneProcessWritesAndReadsExactSameData();
     // testTwoOneProcessWritesTheOtherProcessReadsExactSameData();
     // testOneProcessFillsStreamTheOtherProcessTryToWriteReturnsZero();
     // testOneProcessWaitsToReadTheOtherProcessWritesSomeAndNotifiesToTheWaitingProcess();
     // Ensures only one thread waiting.
+    // remove uint8_t*
     TEST_RESULTS();
     return 0;
 }
