bd6219069ba14d38c58b8505ce0cf822e8c38af2
Higepon (MINOWA Taro) <higepon@gmail.com>
2011-06-01 19:22:35 +0900

    (Green) Added and passed testOneProcessWritesTheOtherProcessReadsExactSameData();.

--- mona/test/monapi/stream/stream.cpp
+++ mona/test/monapi/stream/stream.cpp
@@ -12,10 +12,12 @@ static void testOneProcessReadsEmptyStreamImmediatlyReturnsZero()
     EXPECT_EQ(0, s.read(buf, sizeof(buf)));
 }
 
+static const uint8_t TEST_DATA[] = {0x01, 0x02, 0x03};
+static const int DATA_SIZE = sizeof(TEST_DATA);
+static uintptr_t mainThread = THREAD_UNKNOWN;
+
 static void testOneProcessWritesAndReadsExactSameData()
 {
-    const uint8_t TEST_DATA[] = {0x01, 0x02, 0x03};
-    const int DATA_SIZE = sizeof(TEST_DATA);
     Stream s;
     EXPECT_EQ(DATA_SIZE, s.write(TEST_DATA, DATA_SIZE));
     uint8_t buf[DATA_SIZE];
@@ -24,11 +26,43 @@ static void testOneProcessWritesAndReadsExactSameData()
     EXPECT_EQ(0, s.read(buf, DATA_SIZE));
 }
 
+static void notifyDone(uintptr_t dest)
+{
+    ASSERT_EQ(M_OK, Message::send(dest, MSG_OK));
+}
+
+static void waitSubThreadDone(uintptr_t tid)
+{
+    MessageInfo dst, src;
+    src.header = MSG_OK;
+    int ret = Message::receive(&dst, &src, Message::equalsHeader);
+    ASSERT_EQ(M_OK, ret);
+    ASSERT_TRUE(dst.from == tid);
+}
+
+static void __fastcall readSubThread(void* arg)
+{
+    scoped_ptr<Stream> s(Stream::FromHandle((uintptr_t)arg));
+    uint8_t buf[DATA_SIZE];
+    EXPECT_EQ(DATA_SIZE, s->read(buf, DATA_SIZE));
+    EXPECT_EQ(0, memcmp(TEST_DATA, buf, DATA_SIZE));
+    notifyDone(mainThread);
+}
+
+static void testOneProcessWritesTheOtherProcessReadsExactSameData()
+{
+    Stream s;
+    ASSERT_EQ(DATA_SIZE, s.write(TEST_DATA, DATA_SIZE));
+    mainThread = System::getThreadID();
+    uintptr_t tid = monapi_thread_create_with_arg(readSubThread, (void*)s.handle());
+    waitSubThreadDone(tid);
+}
+
 int main(int argc, char *argv[])
 {
     testOneProcessReadsEmptyStreamImmediatlyReturnsZero();
     testOneProcessWritesAndReadsExactSameData();
-    // testTwoOneProcessWritesTheOtherProcessReadsExactSameData();
+    testOneProcessWritesTheOtherProcessReadsExactSameData();
     // testOneProcessFillsStreamTheOtherProcessTryToWriteReturnsZero();
     // testOneProcessWaitsToReadTheOtherProcessWritesSomeAndNotifiesToTheWaitingProcess();
     // fromHandle
