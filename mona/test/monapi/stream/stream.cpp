b745d50305602d04a7b68e393841d920d936a8c6
Higepon (MINOWA Taro) <higepon@gmail.com>
2011-06-01 18:30:54 +0900

    Added an empty test for MonAPI::Stream.

---
+++ mona/test/monapi/stream/stream.cpp
@@ -0,0 +1,12 @@
+#include <monapi.h>
+
+#define MUNIT_GLOBAL_VALUE_DEFINED
+#include <monapi/MUnit.h>
+
+using namespace MonAPI;
+
+int main(int argc, char *argv[])
+{
+    TEST_RESULTS();
+    return 0;
+}
