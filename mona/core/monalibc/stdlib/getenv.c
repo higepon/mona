558ef7aaeb3f9c28bde0a1c99185b86255e9978e
Higepon (MINOWA Taro) <higepon@gmail.com>
2011-06-28 23:17:01 +0900

    Added /USER/LIB to Mosh default loadpath.

--- mona/core/monalibc/stdlib/getenv.c
+++ mona/core/monalibc/stdlib/getenv.c
@@ -4,6 +4,8 @@ char *getenv(const char *name)
 {
     if (strcmp(name, "HOME") == 0) {
         return "/USER";
+    } else if (strcmp(name, "MOSH_LOADPATH") == 0) {
+      return "/USER/LIB";
     } else {
         return NULL;
     }
