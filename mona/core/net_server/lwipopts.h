8a01accaacf35638a5420a3529d29d319523e7e0
Higepon (MINOWA Taro) <higepon@gmail.com>
2012-02-22 22:44:44 -0800

    Increased PBUF_POOL_SIZE for WebKit.

--- mona/core/net_server/lwipopts.h
+++ mona/core/net_server/lwipopts.h
@@ -131,7 +131,7 @@ a lot of data that needs to be copied, this should be set high. */
 
 /* ---------- Pbuf options ---------- */
 /* PBUF_POOL_SIZE: the number of buffers in the pbuf pool. */
-#define PBUF_POOL_SIZE          100
+#define PBUF_POOL_SIZE          10000
 
 /* PBUF_POOL_BUFSIZE: the size of each pbuf in the pbuf pool. */
 #define PBUF_POOL_BUFSIZE       128
